cf8e7a9a7667c81801a47d01be2469a6ce5a2f96
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-04 15:58:35 +0100

    Celery example #3: configuration

---
+++ celery/example03/enqueue_work.py
@@ -0,0 +1,11 @@
+from tasks import add
+
+result = add.delay(1, 2)
+print(result)
+print(result.backend)
+print(result.ready())
+print(result.get())
+
+print(result.get(timeout=5))
+
+result.forget()
