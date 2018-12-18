44497e63d9102c8e6c8d4be55d189485c613a338
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-12-18 12:04:04 +0100

    2nd example: minor refactoring

---
+++ rabbit-mq/python/example02/consumer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from rabbitmq_connect import connect
+
+connection, channel = connect()
+
+def on_receive(ch, method, properties, body):
+    print("Received %r" % body)
+
+
+channel.basic_consume(on_receive,
+                      queue='test',
+                      no_ack=True)
+
+print('Waiting for messages. To exit press CTRL+C')
+print("...")
+channel.start_consuming()
