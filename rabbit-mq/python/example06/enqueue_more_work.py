27cf52cea3394579924cf179eb90e15fbce27a8d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-02 14:40:51 +0100

    Example 6: use specific queues to use

---
+++ rabbit-mq/python/example06/enqueue_more_work.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from rabbitmq_connect import connect, open_channel
+
+connection = connect()
+channel = open_channel(connection)
+
+for i in range(1, 11):
+    channel.basic_publish(exchange='',
+                          routing_key='test',
+                          body='Hello World! #{i}'.format(i=i))
+
+print("Sent 'Hello World!' ten times")
+connection.close()
---
+++ rabbit-mq/python/example06/enqueue_work.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from rabbitmq_connect import connect, open_channel
+
+connection = connect()
+channel = open_channel(connection)
+
+channel.basic_publish(exchange='',
+                      routing_key='test',
+                      body='Hello World!')
+
+print("Sent 'Hello World!'")
+connection.close()
