82644f11d9b44f7f7e6fa9ae873088df950ed7b2
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-03 10:40:03 +0100

    Example 9: fanout configuration

---
+++ rabbit-mq/python/example09/enqueue_more_work.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from sys import exit, argv
+from rabbitmq_connect import connect, open_channel, use_fanout, bind_queue
+
+
+def run_producer():
+    connection = connect()
+    channel = open_channel(connection)
+
+    use_fanout(channel)
+    bind_queue(channel, 'fronta1')
+    bind_queue(channel, 'fronta2')
+    bind_queue(channel, 'fronta3')
+
+    for i in range(1, 11):
+        channel.basic_publish(exchange='fanout_exchange',
+                              routing_key='',
+                              body='Hello World! #{i}'.format(i=i))
+
+    print('Sent \'Hello World!\' ten times into three queues "fronta1", "fronta2", and "fronta3"')
+    connection.close()
+
+
+run_producer()
