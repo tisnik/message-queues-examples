08f6145c69eb173574e77797581ad5b03f10778d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-03 10:39:45 +0100

    Example 8: use of named queues specified on CLI

---
+++ rabbit-mq/python/example08/enqueue_more_work.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from sys import exit, argv
+from rabbitmq_connect import connect, open_channel
+
+
+def run_producer(queue_name):
+    connection = connect()
+    channel = open_channel(connection)
+
+    for i in range(1, 11):
+        channel.basic_publish(exchange='',
+                              routing_key=queue_name,
+                              body='Hello World! #{i}'.format(i=i))
+
+    print('Sent \'Hello World!\' ten times into the queue "{q}"'.format(q=queue_name))
+    connection.close()
+
+
+if len(argv) <= 1:
+    print('Please provide queue name on the CLI')
+    exit(1)
+
+run_producer(argv[1])
---
+++ rabbit-mq/python/example08/enqueue_work.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from sys import exit, argv
+from rabbitmq_connect import connect, open_channel
+
+
+def run_producer(queue_name):
+    connection = connect()
+    channel = open_channel(connection, queue_name)
+
+    channel.basic_publish(exchange='',
+                          routing_key=queue_name,
+                          body='Hello World!')
+
+    print('Sent \'Hello World!\' message into the queue "{q}"'.format(q=queue_name))
+
+    connection.close()
+
+
+if len(argv) <= 1:
+    print('Please provide queue name on the CLI')
+    exit(1)
+
+run_producer(argv[1])
