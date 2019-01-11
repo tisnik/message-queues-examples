6d6d3449da6b4381b915bdd5dc107dbba7f1a336
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-11 15:20:40 +0100

    Example 5: REQ-RESP pattern

---
+++ 0mq/python/example05/client.py
@@ -0,0 +1,36 @@
+import zmq
+
+
+def connect(port, connection_type):
+    """Otevření socketu se specifikovaným typem spojení."""
+    context = zmq.Context()
+    socket = context.socket(connection_type)
+    address = "tcp://localhost:{port}".format(port=port)
+    socket.connect(address)
+    print("Connected to {a}".format(a=address))
+    return socket
+
+
+def send_request(socket, request):
+    """Poslání požadavku."""
+    print("Sending request '{r}'".format(r=request))
+    socket.send_string(request)
+
+
+def start_client():
+    socket = connect(5556, zmq.REQ)
+
+    send_request(socket, "1")
+    print(socket.recv_string())
+    print()
+
+    send_request(socket, "10")
+    print(socket.recv_string())
+    print()
+
+    send_request(socket, "xyzzy")
+    print(socket.recv_string())
+    print()
+
+
+start_client()
---
+++ 0mq/python/example05/server.py
@@ -0,0 +1,41 @@
+import zmq
+from math import factorial
+
+
+def bind(port, connection_type):
+    """Otevření socketu se specifikovaným typem spojení."""
+    context = zmq.Context()
+    socket = context.socket(connection_type)
+    address = "tcp://*:{port}".format(port=port)
+    socket.bind(address)
+    print("Bound to address {a}".format(a=address))
+    return socket
+
+
+def send_response(socket, response):
+    """Odeslání odpovědi."""
+    print("Sending response '{r}'".format(r=response))
+    socket.send_string(response)
+
+
+def receive_request(socket):
+    """Zpracování požadavku klienta."""
+    request = socket.recv_string()
+    print("Received request from client: '{r}'".format(r=request))
+    return request
+
+
+def start_server():
+    """Spuštění serveru."""
+    socket = bind(5556, zmq.REP)
+    while True:
+        request = receive_request(socket)
+        try:
+            n = int(request)
+            fact = factorial(n)
+            send_response(socket, "{n}! = {f}".format(n=n, f=fact))
+        except Exception as e:
+            send_response(socket, "Wrong input")
+
+
+start_server()
