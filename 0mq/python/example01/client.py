f035af6f3a0fce3dfd58254fe09e63d792a38558
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-11 14:40:51 +0100

    Example 1: PAIR pattern

---
+++ 0mq/python/example01/client.py
@@ -0,0 +1,23 @@
+import zmq
+import sys
+import time
+
+
+def connect(port, connection_type):
+    """Otevření socketu se specifikovaným typem spojení."""
+    context = zmq.Context()
+    socket = context.socket(connection_type)
+    address = "tcp://localhost:{port}".format(port=port)
+    socket.connect(address)
+    return socket
+
+
+def start_client():
+    socket = connect(5556, zmq.PAIR)
+    print("Waiting for messages...")
+    while True:
+        message = socket.recv_string()
+        print("Received message '{m}'".format(m=message))
+
+
+start_client()
---
+++ 0mq/python/example01/server.py
@@ -0,0 +1,29 @@
+import zmq
+import sys
+import time
+
+
+def bind(port, connection_type):
+    """Otevření socketu se specifikovaným typem spojení."""
+    context = zmq.Context()
+    socket = context.socket(connection_type)
+    address = "tcp://*:{port}".format(port=port)
+    socket.bind(address)
+    return socket
+
+
+def send_message(socket, message):
+    """Poslání zprávy."""
+    print("Sending message '{m}'".format(m=message))
+    socket.send_string(message)
+
+
+def start_server():
+    """Spuštění serveru."""
+    socket = bind(5556, zmq.PAIR)
+    for i in range(10):
+        send_message(socket, "Message #{i}".format(i=i))
+        time.sleep(1)
+
+
+start_server()
