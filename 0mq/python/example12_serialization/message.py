e712af29bf2ed902c6e35dc0524c42d446a752b9
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-21 09:32:51 +0100

    Example 12: Python objects serialization and deserialization

---
+++ 0mq/python/example12_serialization/message.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+
+class Message():
+    def __init__(self, number):
+        self.number = number
+        self.timestamp = str(datetime.now())
+        self.message = "Message"
---
+++ 0mq/python/example12_serialization/server.py
@@ -0,0 +1,32 @@
+import zmq
+from zmq.decorators import context, socket
+import time
+from message import Message
+
+
+CONNECTION_TYPE = zmq.PAIR
+PORT = 5556
+
+
+def send_serialized_object(socket, obj, i):
+    """Poslání zprávy."""
+    print("Sending message #{i}'".format(i=i))
+    socket.send_pyobj(obj)
+
+
+@context()
+@socket(CONNECTION_TYPE)
+def start_server(context, socket):
+    """Spuštění serveru."""
+
+    address = "tcp://*:{port}".format(port=PORT)
+    socket.bind(address)
+    print("Bound to address {a}".format(a=address))
+
+    for i in range(10):
+        m = Message(i)
+        send_serialized_object(socket, m, i)
+        time.sleep(1)
+
+
+start_server()
