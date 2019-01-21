1a3567d2fc71fcd5c2c27d595e669260253b9085
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-21 09:31:17 +0100

    Example 8: usage of context managers and with blocks

---
+++ 0mq/python/example08_context_managers/client.py
@@ -0,0 +1,23 @@
+import zmq
+
+
+CONNECTION_TYPE = zmq.PAIR
+PORT = 5556
+
+
+def start_client():
+    """Spuštění klienta."""
+
+    with zmq.Context() as context:
+        with context.socket(CONNECTION_TYPE) as socket:
+            address = "tcp://localhost:{port}".format(port=PORT)
+            socket.connect(address)
+            print("Connected to {a}".format(a=address))
+
+            print("Waiting for message...")
+            while True:
+                message = socket.recv_string()
+                print("Received message '{m}'".format(m=message))
+
+
+start_client()
