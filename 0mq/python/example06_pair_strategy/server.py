985b16983b8c8ed3b848e473ab66c2b4b97b1bd6
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-21 09:30:42 +0100

    Example 6: server+client using PAIR strategy, without proper resource closing

---
+++ 0mq/python/example06_pair_strategy/server.py
@@ -0,0 +1,28 @@
+import zmq
+import time
+
+
+CONNECTION_TYPE = zmq.PAIR
+PORT = 5556
+
+
+def send_message(socket, message):
+    """Poslání zprávy."""
+    print("Sending message '{m}'".format(m=message))
+    socket.send_string(message)
+
+
+def start_server():
+    """Spuštění serveru."""
+    context = zmq.Context()
+    socket = context.socket(CONNECTION_TYPE)
+    address = "tcp://*:{port}".format(port=PORT)
+    socket.bind(address)
+    print("Bound to address {a}".format(a=address))
+
+    for i in range(10):
+        send_message(socket, "Message #{i}".format(i=i))
+        time.sleep(1)
+
+
+start_server()
