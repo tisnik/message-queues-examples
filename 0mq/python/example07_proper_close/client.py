3be867ce6e0f2bb5ad97382f722b55435f1e837e
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-21 09:30:59 +0100

    Example 7: server+client using PAIR strategy, with explicit resource closing

---
+++ 0mq/python/example07_proper_close/client.py
@@ -0,0 +1,37 @@
+import zmq
+
+
+CONNECTION_TYPE = zmq.PAIR
+PORT = 5556
+
+
+def start_client():
+    """Spuštění klienta."""
+    context = zmq.Context()
+
+    try:
+        socket = context.socket(CONNECTION_TYPE)
+        address = "tcp://localhost:{port}".format(port=PORT)
+        socket.connect(address)
+        try:
+            print("Connected to {a}".format(a=address))
+
+            print("Waiting for message...")
+            while True:
+                message = socket.recv_string()
+                print("Received message '{m}'".format(m=message))
+        except Exception as e:
+            print(e)
+        finally:
+            print("Closing socket")
+            socket.close()
+            print("Closed")
+    except Exception as e:
+        print(e)
+    finally:
+        print("Terminating context")
+        context.term()
+        print("Terminated")
+
+
+start_client()
---
+++ 0mq/python/example07_proper_close/server.py
@@ -0,0 +1,43 @@
+import zmq
+import time
+
+
+CONNECTION_TYPE = zmq.PAIR
+PORT = 5556
+
+
+def send_message(socket, message):
+    """Poslání zprávy."""
+    print("Sending message '{m}'".format(m=message))
+    socket.send_string(message)
+
+
+def start_server():
+    """Spuštění serveru."""
+    context = zmq.Context()
+
+    try:
+        socket = context.socket(CONNECTION_TYPE)
+        address = "tcp://*:{port}".format(port=PORT)
+        socket.bind(address)
+        try:
+            print("Bound to address {a}".format(a=address))
+
+            for i in range(10):
+                send_message(socket, "Message #{i}".format(i=i))
+                time.sleep(1)
+        except Exception as e:
+            print(e)
+        finally:
+            print("Closing socket")
+            socket.close()
+            print("Closed")
+    except Exception as e:
+        print(e)
+    finally:
+        print("Terminating context")
+        context.term()
+        print("Terminated")
+
+
+start_server()
