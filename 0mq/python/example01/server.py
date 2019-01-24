cc3e53817c4633fbebfd6ed32e319a64e4f09547
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-24 09:28:22 +0100

    Added comments, the simple client-server example using PAIR sockets

--- 0mq/python/example01/server.py
+++ 0mq/python/example01/server.py
@@ -1,3 +1,13 @@
+# vim: set fileencoding=utf-8
+
+# ---------------------------------------------------------------------
+#
+# Demonstrační příklady využívající knihovnu PyZMQ založenou na 0MQ.
+#
+# Příklad číslo 1: server s komunikační strategií PAIR.
+#
+# ---------------------------------------------------------------------
+
 import zmq
 import time
 
