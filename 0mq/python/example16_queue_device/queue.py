123d5fe773dae1df47fa981ed154fe308de941b5
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-01-25 11:27:28 +0100

    Example 16: usage of queue device

---
+++ 0mq/python/example16_queue_device/queue.py
@@ -0,0 +1,24 @@
+import zmq
+
+XREP_PORT = 5556
+XREQ_PORT = 5557
+
+
+def create_queue(xrep_port, xreq_port):
+    """Vytvoření fronty."""
+    context = zmq.Context()
+
+    frontend = context.socket(zmq.XREP)
+    address = "tcp://*:{port}".format(port=xrep_port)
+    frontend.bind(address)
+    print("Bound to {a} on port {p}".format(a=address, p=xrep_port))
+
+    backend = context.socket(zmq.XREQ)
+    address = "tcp://*:{port}".format(port=xreq_port)
+    backend.bind(address)
+    print("Bound to {a} on port {p}".format(a=address, p=xreq_port))
+
+    zmq.device(zmq.QUEUE, frontend, backend)
+
+
+create_queue(5556, 5557)
---
+++ 0mq/python/example16_queue_device/server.py
@@ -0,0 +1,43 @@
+import zmq
+from math import factorial
+
+PORT = 5557
+
+
+def connect(port, connection_type):
+    """Otevření socketu se specifikovaným typem spojení."""
+    context = zmq.Context()
+    socket = context.socket(connection_type)
+    address = "tcp://localhost:{port}".format(port=port)
+    socket.connect(address)
+    print("Connected to {a}".format(a=address))
+    return socket
+
+
+def send_response(socket, response):
+    """Odeslání odpovědi."""
+    print("Sending response '{r}'".format(r=response))
+    socket.send_string(response)
+
+
+def receive_request(socket):
+    """Zpracování požadavku klienta."""
+    request = socket.recv_string()
+    print("Received request from client: '{r}'".format(r=request))
+    return request
+
+
+def start_server():
+    """Spuštění serveru."""
+    socket = connect(PORT, zmq.REP)
+    while True:
+        request = receive_request(socket)
+        try:
+            n = int(request)
+            fact = factorial(n)
+            send_response(socket, "{n}! = {f}".format(n=n, f=fact))
+        except Exception as e:
+            send_response(socket, "Wrong input")
+
+
+start_server()
