ba0bcf5fb89c369afe70d66468687f48ec1b677c
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-05 09:50:13 +0100

    Correct function name

--- 0mq/python/example23_push_pull_streamer/streamer.py
+++ 0mq/python/example23_push_pull_streamer/streamer.py
@@ -8,7 +8,7 @@ PUSH_PORT = 5551
 @context()
 @socket(zmq.PULL)
 @socket(zmq.PUSH)
-def create_queue(pull_port, push_port, context, frontend, backend):
+def create_streamer(pull_port, push_port, context, frontend, backend):
     """Vytvoření streameru."""
     context = zmq.Context()
 
@@ -23,4 +23,4 @@ def create_queue(pull_port, push_port, context, frontend, backend):
     zmq.device(zmq.STREAMER, frontend, backend)
 
 
-create_queue(PULL_PORT, PUSH_PORT)
+create_streamer(PULL_PORT, PUSH_PORT)
