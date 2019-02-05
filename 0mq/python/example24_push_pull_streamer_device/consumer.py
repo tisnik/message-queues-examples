2e423dbe3851b8609d88393cf305dd8196c22e1a
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-05 09:50:50 +0100

    Example 24: PUSH-PULL and Streamer device

---
+++ 0mq/python/example24_push_pull_streamer_device/consumer.py
@@ -0,0 +1,28 @@
+import zmq
+from zmq.decorators import context, socket
+import time
+
+
+CONNECTION_TYPE = zmq.PULL
+PORT = 5551
+
+
+@context()
+@socket(CONNECTION_TYPE)
+def start_consumer(port, context, socket):
+    """Spuštění konzumenta."""
+
+    address = "tcp://localhost:{port}".format(port=port)
+    socket.connect(address)
+    print("Connected to {a}".format(a=address))
+
+    print("Waiting for message...")
+    cnt = 0
+    while True:
+        message = socket.recv_string()
+        cnt += 1
+        print("Received message {c} of 100: '{m}'".format(c=cnt, m=message))
+        time.sleep(0)
+
+
+start_consumer(PORT)
---
+++ 0mq/python/example24_push_pull_streamer_device/streamer.py
@@ -0,0 +1,56 @@
+import zmq
+from zmq.decorators import context, socket
+from zmq.devices.basedevice import ProcessDevice
+import time
+
+PULL_PORT = 5550
+PUSH_PORT = 5551
+
+PRODUCER_CONNECTION_TYPE = zmq.PUSH
+PRODUCER_PORT = 5550
+
+
+def create_streamer(pull_port, push_port):
+    """Vytvoření streameru."""
+    streamer_device = ProcessDevice(zmq.STREAMER, zmq.PULL, zmq.PUSH)
+
+    frontend_address = "tcp://*:{port}".format(port=pull_port)
+    backend_address = "tcp://*:{port}".format(port=push_port)
+
+    streamer_device.bind_in(frontend_address)
+    streamer_device.bind_out(backend_address)
+
+    print("Bound to {a} on port {p}".format(a=frontend_address, p=pull_port))
+    print("Bound to {a} on port {p}".format(a=backend_address, p=push_port))
+
+    streamer_device.start()
+    print("Device started in background")
+
+
+def send_message(socket, message):
+    """Poslání zprávy."""
+    print("Sending message '{m}'".format(m=message))
+    socket.send_string(message)
+
+
+@socket(PRODUCER_CONNECTION_TYPE)
+def start_producer(port, context, socket):
+    """Spuštění zdroje zprav."""
+
+    address = "tcp://localhost:{port}".format(port=port)
+    # socket.set_hwm(1)
+    socket.connect(address)
+    print("Connected to address {a}".format(a=address))
+
+    for i in range(100):
+        send_message(socket, "Message #{i}".format(i=i))
+        time.sleep(0.2)
+
+
+@context()
+def start_device_and_producer(context):
+    create_streamer(PULL_PORT, PUSH_PORT)
+    start_producer(PRODUCER_PORT, context)
+
+
+start_device_and_producer()
