142363a0def9771dd9bb2eea7ac66c20d41cff3f
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-08 14:51:30 +0100

    Example 4: ACKing message

---
+++ amq/example04_ack/subscriber.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import time
+import stomp
+
+
+class SimpleListener(object):
+
+    def __init__(self, conn):
+        self.conn = conn
+
+    def on_message(self, headers, message):
+        print("Received message: {m}".format(m=message))
+        print(headers)
+        message_id = headers['message-id']
+        subscription = headers['subscription']
+        self.conn.ack(message_id, subscription)
+
+    def on_error(self, headers, message):
+        print("Received an error {e}".format(e=message))
+
+
+destination = "/queue/test"
+
+conn = stomp.Connection(host_and_ports=[("localhost", 61613)])
+conn.set_listener('', SimpleListener(conn))
+conn.start()
+
+conn.connect(login="admin", passcode="admin")
+conn.subscribe(id='simple_listener', destination=destination, ack='client')
+
+print("Waiting for messages...")
+
+while True:
+    time.sleep(10)
