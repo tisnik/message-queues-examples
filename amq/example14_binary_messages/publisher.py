157794c213e37dfe4fa9b17dab65a701a1f9d2d8
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-20 09:09:31 +0100

    Sending and receiving binary messages

---
+++ amq/example14_binary_messages/publisher.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import time
+import stomp
+import base64
+
+
+destination = "/queue/test"
+
+MESSAGES = 10
+
+conn = stomp.Connection(host_and_ports=[("localhost", 61613)])
+conn.start()
+conn.connect(login="admin", passcode="admin")
+
+
+with open("vim_editor.gif", mode="rb") as file:
+    binaryData = file.read()
+    print("Read {l} bytes from binary file".format(l=len(binaryData)))
+
+    textData = base64.b64encode(binaryData)
+    conn.send(destination, textData, persistent='true')
+    print("Sent {l} characters".format(l=len(textData)))
+
+
+conn.disconnect()
---
+++ amq/example14_binary_messages/subscriber.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import time
+import stomp
+import base64
+
+
+class SimpleListener:
+
+    def __init__(self, conn):
+        self.conn = conn
+
+    def on_message(self, headers, message):
+        binaryData = base64.b64decode(message)
+        print("Received {l} characters".format(l=len(message)))
+        print("   converted into {l} bytes".format(l=len(binaryData)))
+
+        with open("output.gif", "wb") as f:
+            f.write(binaryData)
+
+    def on_error(self, headers, message):
+        print("Received an error {e}".format(e=message))
+
+
+destination = "/queue/test"
+
+conn = stomp.Connection(host_and_ports=[("localhost", 61613)])
+conn.set_listener('', SimpleListener(conn))
+conn.start()
+
+conn.connect(login="admin", passcode="admin")
+conn.subscribe(id='simple_listener', destination=destination, ack='auto')
+
+print("Waiting for messages...")
+
+while True:
+    time.sleep(10)
