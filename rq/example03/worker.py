39833e0094b2ebe4d31a0b030206f783777c4fc4
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-02-28 08:58:42 +0100

    Links to article about Redis MQ added to the 3rd example

--- rq/example03/worker.py
+++ rq/example03/worker.py
@@ -1,5 +1,8 @@
 # vim: set fileencoding=utf-8
 
+# This script is used and described in the following article:
+# https://www.root.cz/clanky/pouziti-nastroje-rq-redis-queue-pro-spravu-uloh-zpracovavanych-na-pozadi/
+
 from time import sleep
 
 
