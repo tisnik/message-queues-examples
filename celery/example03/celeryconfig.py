a9c94adf4ab3804a60437ef267019bc37e20fed8
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-13 11:54:29 +0100

    Added file encoding specification into example #3

--- celery/example03/celeryconfig.py
+++ celery/example03/celeryconfig.py
@@ -1,3 +1,5 @@
+# vim: set fileencoding=utf-8
+
 broker_url = 'redis://localhost:6379/0'
 result_backend = 'redis://localhost:6379/0'
 
--- celery/example03/tasks.py
+++ celery/example03/tasks.py
@@ -1,3 +1,5 @@
+# vim: set fileencoding=utf-8
+
 from celery import Celery
 
 app = Celery('tasks')
