e3e2d969edc8168b417246c7b21a947e58dc5e9c
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-08 08:36:40 +0200

    Example 3: usage of one replay function - DeliverAllAvailable

---
+++ go-nats-streaming/03-replay-all/consumer.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	streaming "github.com/nats-io/go-nats-streaming"
+	"log"
+)
+
+const clusterId = "test-cluster"
+const clientId = "consumer1"
+const topic = "topic1"
+
+func onReceive(m *streaming.Msg) {
+	println("Received message: ", string(m.Data))
+}
+
+func main() {
+	stream, err := streaming.Connect(clusterId, clientId)
+	if err != nil {
+		log.Fatalf("Can not connect to cluster %s\n", clusterId)
+	}
+	defer stream.Close()
+
+	c := make(chan bool)
+
+	startOpt := streaming.DeliverAllAvailable()
+	sub, err := stream.Subscribe(topic, onReceive, startOpt)
+	if err != nil {
+		log.Fatalf("Can not subscribe to topic %s\n", topic)
+	}
+	defer sub.Unsubscribe()
+
+	<-c
+}
