a525b49d9c6130c9134dc1a32add09c919e8dfbd
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-17 07:47:15 +0200

    Example 5: nanomsg behaviour when the wrong socked type is used

---
+++ nanomsg/05_pair_communication_wrong_socket/second.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <nanomsg/nn.h>
+#include <nanomsg/pipeline.h>
+
+const char *URL = "ipc:///tmp/example5";
+
+void report_error(const char *func)
+{
+    fprintf(stderr, "%s: %s\n", func, nn_strerror(nn_errno()));
+    exit(1);
+}
+
+void receive_message(socket)
+{
+    char *message = NULL;
+    int bytes;
+
+    if ((bytes = nn_recv(socket, &message, NN_MSG, 0)) < 0) {
+        report_error("nn_recv");
+    }
+    printf("Received message '%s' with length %d bytes\n", message, bytes); 
+    if (nn_freemsg(message) < 0) {
+        report_error("nn_freemsg");
+    }
+}
+
+void send_response(const int socket, const char *response)
+{
+    int bytes;
+
+    printf("Sending response '%s'\n", response);
+    if ((bytes = nn_send(socket, response, strlen(response)+1, 0)) < 0) {
+        report_error("nn_send");
+    }
+    printf("Response with length %d bytes sent, flushing\n", bytes);
+    sleep(1);
+}
+
+void second(const char *url)
+{
+    int socket;
+    int endpoint;
+
+    if ((socket = nn_socket(AF_SP, NN_PUSH)) < 0) {
+        report_error("nn_socket");
+    }
+    puts("Socket created");
+
+    if ((endpoint = nn_bind(socket, url)) < 0) {
+        report_error("nn_bind");
+    }
+    puts("Endpoint bound to socket");
+
+    puts("Waiting for message...");
+    receive_message(socket);
+
+    send_response(socket, "ACK!");
+
+    if (nn_shutdown(socket, endpoint) < 0) {
+        report_error("nn_shutdown");
+    }
+    puts("Shutdown completed");
+}
+
+int main(int argc, char **argv)
+{
+    second(URL);
+    return 0;
+}
