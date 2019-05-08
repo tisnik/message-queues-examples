b13fa11547bf5d6469005d4a19d8e21abdd1fbc3
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-08 09:01:23 +0200

    Example 9: setting topic to filter incoming messages

---
+++ nanomsg-python/09_pub_sub_topic/publisher.py
@@ -0,0 +1,19 @@
+from time import sleep
+from nanomsg import Socket, PUB
+
+URL = "ipc:///tmp/example9"
+
+
+with Socket(PUB) as socket:
+    print("Socket created")
+
+    socket.bind(URL)
+    print("Bound to URL {}".format(URL))
+
+    for i in range(1, 1000):
+        message = "Message #{}".format(i)
+        print("Publishing message {}".format(message))
+        socket.send(message)
+        sleep(0.5)
+
+print("Socket closed")
