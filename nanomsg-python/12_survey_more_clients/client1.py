6ef0cc40a6e3b983cd5741d6edc09ae3f52b8e1e
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-08 14:30:50 +0200

    Example #12: survey with more clients

---
+++ nanomsg-python/12_survey_more_clients/client1.py
@@ -0,0 +1,35 @@
+from nanomsg import Socket, RESPONDENT
+from random import seed, randint
+
+URL = "ipc:///tmp/example12"
+
+
+def receive_question(socket):
+    question = socket.recv()
+    print("Received question: '{}'".format(question))
+
+
+def send_answer(socket, answer):
+    print("Sending answer: '{}'".format(answer))
+    socket.send(answer)
+
+
+seed(None)
+
+with Socket(RESPONDENT) as socket:
+    print("Socket created")
+
+    socket.connect(URL)
+    print("Connected to URL {}".format(URL))
+
+    while True:
+        receive_question(socket)
+        print("Question received")
+
+        number = randint(0, 100)
+        answer = "Hello, I'm client #1. It must be {}".format(number)
+
+        send_answer(socket, answer)
+        print("Answer sent")
+
+print("Socket closed")
---
+++ nanomsg-python/12_survey_more_clients/server.py
@@ -0,0 +1,41 @@
+from nanomsg import Socket, SURVEYOR
+from time import sleep
+
+URL = "ipc:///tmp/example12"
+
+
+def send_survey(socket, message):
+    socket.send(message)
+
+
+def receive_answer(socket):
+    answer = socket.recv()
+    print("Received answer: '{}'".format(answer))
+
+
+def wait_for_clients(seconds):
+    print("Waiting for clients to connect...")
+    for i in range(seconds, 0, -1):
+        print(i)
+        sleep(1)
+
+
+with Socket(SURVEYOR) as socket:
+    print("Socket created")
+
+    socket.bind(URL)
+    print("Bound to URL {}".format(URL))
+
+    wait_for_clients(10)
+
+    send_survey(socket, "What do you get when you multiply six by nine?")
+    print("Survey send, waiting for answers...")
+
+    answers = 0
+
+    while True:
+        receive_answer(socket)
+        answers += 1
+        print("Processed {} answers so far".format(answers))
+
+print("Socket closed")
