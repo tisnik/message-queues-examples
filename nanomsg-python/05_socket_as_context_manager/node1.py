4b485ec54960169834e747f97ab4c528cbc4fbd6
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-08 09:01:23 +0200

    Example 5: use socket object as a context manager

---
+++ nanomsg-python/05_socket_as_context_manager/node1.py
@@ -0,0 +1,17 @@
+from nanomsg import Socket, PAIR
+
+URL = "ipc:///tmp/example5"
+
+with Socket(PAIR) as socket:
+    print("Socket created")
+
+    socket.bind(URL)
+    print("Bound to URL {}".format(URL))
+
+    socket.send("Hello world!")
+    print("Message has been sent")
+
+    message = socket.recv()
+    print("Received response: {}".format(message))
+
+print("Socket closed")
---
+++ nanomsg-python/05_socket_as_context_manager/node2.py
@@ -0,0 +1,18 @@
+from nanomsg import Socket, PAIR
+
+URL = "ipc:///tmp/example5"
+
+with Socket(PAIR) as socket:
+    socket = Socket(PAIR)
+    print("Socket created")
+
+    socket.connect(URL)
+    print("Connected to URL {}".format(URL))
+
+    message = socket.recv()
+    print("Received: {}".format(message))
+
+    socket.send("Thanks for your message '{}'".format(message))
+    print("Response has been sent")
+
+print("Socket closed")
