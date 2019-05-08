973425d5173c13a01fad8c34211042de6174ca76
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-08 09:01:23 +0200

    Example 10: more consumers, each with different filter

---
+++ nanomsg-python/10_pub_sub_topics/publisher.py
@@ -0,0 +1,23 @@
+from time import sleep
+from nanomsg import Socket, PUB
+
+URL = "ipc:///tmp/example10"
+
+
+with Socket(PUB) as socket:
+    print("Socket created")
+
+    socket.bind(URL)
+    print("Bound to URL {}".format(URL))
+
+    for i in range(1, 1000):
+        message = "Message A #{}".format(i)
+        print("Publishing message {}".format(message))
+        socket.send(message)
+        sleep(0.5)
+        message = "Message B #{}".format(i)
+        print("Publishing message {}".format(message))
+        socket.send(message)
+        sleep(0.5)
+
+print("Socket closed")
---
+++ nanomsg-python/10_pub_sub_topics/subscriberA.py
@@ -0,0 +1,22 @@
+from time import sleep
+from nanomsg import Socket, SUB, SUB_SUBSCRIBE
+
+URL = "ipc:///tmp/example10"
+
+
+def receive_message(socket):
+    message = socket.recv()
+    print("Received message: '{}'".format(message))
+
+
+with Socket(SUB) as socket:
+    print("Socket created")
+
+    socket.connect(URL)
+    socket.set_string_option(SUB, SUB_SUBSCRIBE, "Message A")
+    print("Connected to URL {}".format(URL))
+
+    while True:
+        receive_message(socket)
+
+print("Socket closed")
