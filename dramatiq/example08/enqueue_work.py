8d48523a9a5060c4c36aecc8d66e5a40b1a551e6
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-24 07:55:41 +0200

    Dramatiq example #8: workers group + result store

---
+++ dramatiq/example08/enqueue_work.py
@@ -0,0 +1,18 @@
+import time
+import dramatiq
+
+from dramatiq.brokers.redis import RedisBroker
+
+from test_worker_8 import test_worker_A, test_worker_B, test_worker_C, setup_broker_and_backend
+
+
+setup_broker_and_backend()
+
+for i in range(1, 6):
+    print(i)
+    g = dramatiq.group([
+        test_worker_A.message(str(i)),
+        test_worker_B.message(str(i)),
+        test_worker_C.message(str(i))
+    ]).run()
+    g.wait(timeout=20000)
