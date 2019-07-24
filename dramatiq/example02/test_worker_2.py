d4234c9e4f085ca7205d641386767667dd3acb85
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-24 07:50:24 +0200

    Dramatiq example #2: worker with parameters

---
+++ dramatiq/example02/test_worker_2.py
@@ -0,0 +1,20 @@
+import time
+
+import dramatiq
+from dramatiq.brokers.redis import RedisBroker
+
+
+def setup_broker():
+    redis_broker = RedisBroker(host="localhost", port=6379)
+    dramatiq.set_broker(redis_broker)
+    return redis_broker
+
+
+setup_broker()
+
+
+@dramatiq.actor
+def test_worker(parameter):
+    print("Working, received parameter: {param}".format(param=parameter))
+    time.sleep(1)
+    print("Done")
