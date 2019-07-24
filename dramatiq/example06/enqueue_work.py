6b5a6ea9c1f9b85890f6fc2f665fa0240347595d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-24 07:54:58 +0200

    Dramatiq example #6: more workers

---
+++ dramatiq/example06/enqueue_work.py
@@ -0,0 +1,14 @@
+import time
+import dramatiq
+
+from dramatiq.brokers.redis import RedisBroker
+
+from test_worker_6 import test_worker_A, test_worker_B, test_worker_C, setup_broker
+
+
+setup_broker()
+
+for i in range(1, 6):
+    test_worker_A.send(i)
+    test_worker_B.send(i)
+    test_worker_C.send(i)
