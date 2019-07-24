8c93b5c9510e75a6f9d1d2193e5e86fc731830b1
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-24 07:50:46 +0200

    Dramatiq example #3: worker that throws an exception

---
+++ dramatiq/example03/enqueue_work.py
@@ -0,0 +1,12 @@
+import time
+import dramatiq
+
+from dramatiq.brokers.redis import RedisBroker
+
+from test_worker_3 import test_worker, setup_broker
+
+
+setup_broker()
+
+for i in range(1, 11):
+    test_worker.send(i)
