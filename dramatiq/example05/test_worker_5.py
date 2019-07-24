1b878dacc62e3455fbdef83488a45612f7bebfda
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-24 07:54:35 +0200

    Dramatiq example #5: min_backoff and max_backoff settings

---
+++ dramatiq/example05/test_worker_5.py
@@ -0,0 +1,21 @@
+import time
+
+import dramatiq
+from dramatiq.brokers.redis import RedisBroker
+
+
+def setup_broker():
+    redis_broker = RedisBroker(host="localhost", port=6379)
+    dramatiq.set_broker(redis_broker)
+    return redis_broker
+
+
+setup_broker()
+
+
+@dramatiq.actor(min_backoff=100, max_backoff=2000)
+def test_worker(parameter):
+    print("Working, received parameter: {param}".format(param=parameter))
+    raise Exception("I don't like this parameter!")
+    time.sleep(1)
+    print("Done")
