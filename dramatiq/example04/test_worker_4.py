a4196a703bfaf6de45a86c91ba5842164cbd670d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-24 07:54:09 +0200

    Dramatiq example #4: max retries setting

---
+++ dramatiq/example04/test_worker_4.py
@@ -0,0 +1,22 @@
+import time
+
+import dramatiq
+from dramatiq.brokers.redis import RedisBroker
+
+
+def setup_broker():
+    redis_broker = RedisBroker(host="localhost", port=6379)
+    dramatiq.set_broker(redis_broker)
+    return redis_broker
+
+
+setup_broker()
+
+
+@dramatiq.actor(max_retries=3)
+def test_worker(parameter):
+    print("Working, received parameter: {param}".format(param=parameter))
+    if parameter % 3 == 0:
+        raise Exception("I don't like this parameter!")
+    time.sleep(1)
+    print("Done")
