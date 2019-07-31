7680740dae9d44a351bc8d3d53f66aaf318f312f
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-31 08:08:40 +0200

    Durable-queue example #4

---
+++ durable-queue/durable-queue-4/src/durable_queue_4/core.clj
@@ -0,0 +1,29 @@
+(ns durable-queue-4.core
+  (:gen-class))
+
+(require '[durable-queue :refer :all])
+(require '[clojure.pprint :refer :all])
+
+(defn deque-and-complete-message
+  [q queue-name]
+  (let [message (take! q queue-name)]
+       (println "Message dequeued" message)
+       (complete! message)
+       (println "Message completed" message)))
+
+(defn -main
+  [& args]
+  (let [q (queues "/tmp" {})]
+       (pprint (stats q))
+       (put! q :queue-1 "task #A")
+       (put! q :queue-1 "task #B")
+       (put! q :queue-2 "task #C")
+       (pprint (stats q))
+       (println "Získávám a kompletuji dvě zprávy z front queue-1 a queue-2")
+       (deque-and-complete-message q :queue-1)
+       (deque-and-complete-message q :queue-2)
+       (pprint (stats q))
+       (println "Získávám a kompletuji další dvě zprávy z front queue-1 a queue-2")
+       (deque-and-complete-message q :queue-1)
+       (deque-and-complete-message q :queue-2)
+       (pprint (stats q))))
---
+++ durable-queue/durable-queue-4/test/durable_queue_4/core_test.clj
@@ -0,0 +1,7 @@
+(ns durable-queue-4.core-test
+  (:require [clojure.test :refer :all]
+            [durable-queue-4.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
