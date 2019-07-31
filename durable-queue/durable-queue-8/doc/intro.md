264d4968c77e55946d356118493a5736117399ee
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-07-31 08:08:40 +0200

    Durable-queue example #8

---
+++ durable-queue/durable-queue-8/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to durable-queue-1
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ durable-queue/durable-queue-8/src/durable_queue_8/core.clj
@@ -0,0 +1,39 @@
+(ns durable-queue-8.core
+  (:gen-class))
+
+(require '[durable-queue :refer :all])
+(require '[clojure.pprint :refer :all])
+
+(defn sleep
+  [amount]
+  (Thread/sleep amount))
+
+(defn worker
+  [q queue-name]
+  (println "Worker started")
+  (loop []
+    (let [message (take! q queue-name)
+          value   (deref message)]
+        (if (=  value :exit)
+            (println "Stopping worker")
+            (do
+                (println "Worker received message" value)
+                (complete! message)
+                (sleep 2000)
+                (println "Worker completed message" value)
+                (recur))))))
+
+(defn -main
+  [& args]
+  (let [q (queues "/tmp" {:max-queue-size 10})]
+       (pprint (stats q))
+       (println "Starting worker")
+       (.start (Thread. (fn [] (worker q :queue-1))))
+       (doseq [i (range 10)]
+           (println "Enqueuing task #" i)
+           (put! q :queue-1 (str "task #" i))
+           (pprint (stats q))
+           (sleep 1000))
+       (println "Enqueuing task to stop worker")
+       (put! q :queue-1 :exit)
+       (println "All tasks has been scheduled")))
