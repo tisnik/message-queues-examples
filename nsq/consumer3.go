e8fbe2592e386355afd92613aaa1493f385cfc5c
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-28 09:56:32 +0100

    Consumer written in Go, directory service usage

---
+++ nsq/consumer3.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/nsqio/go-nsq"
+	"log"
+)
+
+func main() {
+	config := nsq.NewConfig()
+
+	consumer, err := nsq.NewConsumer("test", "test", config)
+	if err != nil {
+		log.Panic("Consumer can't be constructed")
+	}
+
+	done := make(chan bool)
+
+	consumer.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+		log.Printf("Received a message: %s", string(message.Body))
+		return nil
+	}))
+
+	err = consumer.ConnectToNSQLookupd("127.0.0.1:4161")
+	if err != nil {
+		log.Panic("Could not connect")
+	}
+
+	log.Println("Waiting for message")
+	<-done
+}
