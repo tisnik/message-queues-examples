f03cfbd0ce6e05bc9fd88fec27e09a2b3f01cbd6
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-28 09:56:32 +0100

    Consumer written in Python

---
+++ nsq/consumer.py
@@ -0,0 +1,11 @@
+import nsq
+
+def handler(message):
+    print(message)
+    return True
+
+r = nsq.Reader(message_handler=handler,
+        lookupd_http_addresses=['http://127.0.0.1:4161'],
+        topic='test', channel='test', lookupd_poll_interval=15)
+
+nsq.run()
