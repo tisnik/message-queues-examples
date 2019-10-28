4457de7cb1fe44645c9b56d936243ef4483ebe8d
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-28 09:56:32 +0100

    Consumer written in Go, usage of channels

---
+++ nsq/consumer2.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/nsqio/go-nsq"
+	"log"
+)
+
+func main() {
+	config := nsq.NewConfig()
+
+	consumer, err := nsq.NewConsumer("test", "test", config)
+	if err != nil {
+		log.Panic("Consumer can't be constructed")
+	}
+
+	done := make(chan bool)
+
+	consumer.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+		log.Printf("Received a message: %s", string(message.Body))
+		done <- true
+		return nil
+	}))
+
+	err = consumer.ConnectToNSQD("127.0.0.1:4150")
+	if err != nil {
+		log.Panic("Could not connect")
+	}
+
+	log.Println("Waiting for message")
+	<-done
+}
