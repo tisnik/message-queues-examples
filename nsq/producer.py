76277c7bcd708e988c85397bc676b3cdc08d9ea0
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-28 09:56:32 +0100

    Simple producer written in Python

---
+++ nsq/producer.py
@@ -0,0 +1,20 @@
+import nsq
+import tornado.ioloop
+import time
+
+cnt = 1
+
+def pub_message():
+    global cnt
+    writer.pub('test', 'zprava {}'.format(cnt).encode(), finish_pub)
+    cnt += 1
+
+def finish_pub(conn, data):
+    print("FINISHING")
+    print(data.decode())
+    print("---------")
+
+writer = nsq.Writer(['127.0.0.1:4150'])
+
+tornado.ioloop.PeriodicCallback(pub_message, 1000).start()
+nsq.run()
