f241f4f3aac67ef7c771fbaf9a71ad5c245a0fb9
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-12 10:02:25 +0100

    3th version of producer

---
+++ nsq/producer3.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nsqio/go-nsq"
+	"log"
+	"time"
+)
+
+const Address = "127.0.0.1:4150"
+
+const Topic = "test"
+
+func main() {
+	config := nsq.NewConfig()
+
+	producer, err := nsq.NewProducer(Address, config)
+	if err != nil {
+		log.Panic("Producer can't be constructed")
+	}
+	defer producer.Stop()
+
+	i := 0
+
+	for {
+		message := fmt.Sprintf("Zprava z Go #%d", i)
+		log.Print("Sending message: ", message)
+		err = producer.Publish(Topic, []byte(message))
+		if err != nil {
+			log.Panic("Could not connect")
+		}
+		i++
+		time.Sleep(1 * time.Second)
+	}
+}
