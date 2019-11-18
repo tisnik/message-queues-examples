5718cd901313a5ff30b085e415adfae76275f15c
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-18 08:52:40 +0100

    System-V queues example #3: publisher send messages continuously

---
+++ unix-messages/system-v/example3/subscriber.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+typedef struct {
+    long message_type;
+    char message_text[100];
+} t_message;
+
+int main(void)
+{
+    t_message message;
+    key_t key;
+    int queue_id;
+    int status;
+
+    key = ftok("/home/tester/.bashrc", 1234);
+
+    if (key == -1) {
+        perror("Unable to generate key");
+        return 2;
+    }
+
+    printf("Key: %x\n", key);
+
+    queue_id = msgget(key, 0);
+
+    if (queue_id == -1) {
+        perror("Unable to get message queue identifier");
+        return 2;
+    }
+
+    printf("Message queue identifier: %x\n", queue_id);
+
+    while (1) {
+        status = msgrcv(queue_id, (void*)&message, sizeof(message.message_text), 0, 0);
+
+        if (status == -1) {
+            perror("Can not receive message");
+            return 1;
+        }
+
+        printf("Message type: %ld\n", message.message_type);
+        printf("Message text: %s\n", message.message_text);
+    }
+    return 0;
+}
