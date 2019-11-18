78d7d9e5561d3b3eb0b9d2aa4075c89a9b306284
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-11-18 08:52:40 +0100

    System-V queues example #2: publisher for more messages, subscriber to accept them all

---
+++ unix-messages/system-v/example2/publisher.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+typedef struct {
+    long message_type;
+    char message_text[100];
+} t_message;
+
+int main(void)
+{
+    t_message message;
+    key_t key;
+    int queue_id;
+    int status;
+    int msg_number = 1;
+
+    message.message_type = 1;
+
+    key = ftok("/home/tester/.bashrc", 1234);
+
+    if (key == -1) {
+        perror("Unable to generate key");
+        return 2;
+    }
+
+    printf("Key: %x\n", key);
+
+    queue_id = msgget(key, IPC_CREAT | 0660);
+
+    if (queue_id == -1) {
+        perror("Unable to get message queue identifier");
+        return 2;
+    }
+
+    printf("Message queue identifier: %x\n", queue_id);
+
+    while (1) {
+        sprintf(message.message_text, "Message #%d", msg_number);
+        msg_number++;
+        status = msgsnd(queue_id, (void*)&message, sizeof(message.message_text), 0);
+
+        if (status == -1) {
+            perror("Can not send message");
+            return 1;
+        }
+        sleep(1);
+    }
+
+    return 0;
+}
