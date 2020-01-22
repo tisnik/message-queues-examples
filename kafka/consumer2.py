65fddc0f7cf8955679fa239a1226c151aee5212d
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-22 11:13:20 +0100

    Kafka consumer with ability to replay

---
+++ kafka/consumer2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+from kafka import KafkaConsumer, TopicPartition
+
+server = 'localhost:9092'
+topic = 'upload'
+group_id = 'group1'
+
+print('Connecting to Kafka')
+consumer = KafkaConsumer(group_id=group_id,
+                         bootstrap_servers=[server])
+print('Connected to Kafka')
+
+tp = TopicPartition(topic=topic, partition=0)
+consumer.assign([tp])
+consumer.seek(tp, 0)
+
+try:
+    for message in consumer:
+        print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
+except KeyboardInterrupt:
+    sys.exit()
