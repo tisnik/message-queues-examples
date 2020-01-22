7124970cd28896f01b3f85fd7a57c74bb8c33d5c
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-01-22 11:13:02 +0100

    Kafka producer written in Python

---
+++ kafka/producer1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from kafka import KafkaProducer
+from time import sleep
+from json import dumps
+
+server = 'localhost:9092'
+topic = 'upload'
+
+print('Connecting to Kafka')
+producer = KafkaProducer(bootstrap_servers=[server],
+                         value_serializer=lambda x: dumps(x).encode('utf-8'))
+print('Connected to Kafka')
+
+for i in range(1000):
+    data = {'counter': i}
+    producer.send(topic, value=data)
+    sleep(5)
