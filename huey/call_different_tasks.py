7448b19d81b5c753da0b5eb2d199f8317eeec669
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-06-17 08:40:50 +0200

    Calling/planning different tasks

---
+++ huey/call_different_tasks.py
@@ -0,0 +1,17 @@
+"""Vytvoření různých úloh."""
+
+from three_tasks import add, slow_add, mul
+
+rs = []
+
+print("Queueing...")
+
+for i in range(1, 11):
+    rs.append(add(i, i))
+    rs.append(slow_add(i, i))
+    rs.append(mul(i, i))
+
+print("Done, waiting for results...")
+
+for r in rs:
+    print(r(blocking=True))
