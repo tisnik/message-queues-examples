6a1edd146f13b648432f2a4ef73a77adcf4a4776
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-07-16 12:40:46 +0200

    Fixed issue #22

--- nsq/consumer.py
+++ nsq/consumer.py
@@ -6,6 +6,6 @@ def handler(message):
 
 r = nsq.Reader(message_handler=handler,
         lookupd_http_addresses=['http://127.0.0.1:4161'],
-        topic='test', channel='test', lookupd_poll_interval=15)
+               topic='test', channel='test', lookupd_poll_interval=15)
 
 nsq.run()
