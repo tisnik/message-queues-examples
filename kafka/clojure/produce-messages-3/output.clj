572eae21e78e4b86fa72c7b2d32b36d345aac48f
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-01 08:01:16 +0100

    Message producer, create 100 messages with key specification

---
+++ kafka/clojure/produce-messages-3/output.clj
@@ -0,0 +1,700 @@
+Publishing message with key ' 1 ' and value ' Message #1 '
+{:topic-name "test3",
+ :partition 9,
+ :timestamp 1606555678845,
+ :offset 0,
+ :serialized-key-size 1,
+ :serialized-value-size 10}
+Publishing message with key ' 2 ' and value ' Message #2 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555678885,
+ :offset 0,
+ :serialized-key-size 1,
+ :serialized-value-size 10}
+Publishing message with key ' 3 ' and value ' Message #3 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555678891,
+ :offset 0,
+ :serialized-key-size 1,
+ :serialized-value-size 10}
+Publishing message with key ' 4 ' and value ' Message #4 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555678898,
+ :offset 1,
+ :serialized-key-size 1,
+ :serialized-value-size 10}
+Publishing message with key ' 5 ' and value ' Message #5 '
+{:topic-name "test3",
+ :partition 0,
+ :timestamp 1606555678904,
+ :offset 0,
+ :serialized-key-size 1,
+ :serialized-value-size 10}
+Publishing message with key ' 6 ' and value ' Message #6 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555678911,
+ :offset 1,
+ :serialized-key-size 1,
+ :serialized-value-size 10}
+Publishing message with key ' 7 ' and value ' Message #7 '
+{:topic-name "test3",
+ :partition 9,
+ :timestamp 1606555678917,
+ :offset 1,
+ :serialized-key-size 1,
+ :serialized-value-size 10}
+Publishing message with key ' 8 ' and value ' Message #8 '
+{:topic-name "test3",
+ :partition 7,
+ :timestamp 1606555678922,
+ :offset 0,
+ :serialized-key-size 1,
+ :serialized-value-size 10}
+Publishing message with key ' 9 ' and value ' Message #9 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555678928,
+ :offset 2,
+ :serialized-key-size 1,
+ :serialized-value-size 10}
+Publishing message with key ' 10 ' and value ' Message #10 '
+{:topic-name "test3",
+ :partition 6,
+ :timestamp 1606555678932,
+ :offset 0,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 11 ' and value ' Message #11 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555678937,
+ :offset 2,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 12 ' and value ' Message #12 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555678941,
+ :offset 3,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 13 ' and value ' Message #13 '
+{:topic-name "test3",
+ :partition 7,
+ :timestamp 1606555678946,
+ :offset 1,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 14 ' and value ' Message #14 '
+{:topic-name "test3",
+ :partition 5,
+ :timestamp 1606555678950,
+ :offset 0,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 15 ' and value ' Message #15 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555678955,
+ :offset 4,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 16 ' and value ' Message #16 '
+{:topic-name "test3",
+ :partition 1,
+ :timestamp 1606555678959,
+ :offset 0,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 17 ' and value ' Message #17 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555678963,
+ :offset 5,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 18 ' and value ' Message #18 '
+{:topic-name "test3",
+ :partition 2,
+ :timestamp 1606555678968,
+ :offset 0,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 19 ' and value ' Message #19 '
+{:topic-name "test3",
+ :partition 5,
+ :timestamp 1606555678972,
+ :offset 1,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 20 ' and value ' Message #20 '
+{:topic-name "test3",
+ :partition 2,
+ :timestamp 1606555678976,
+ :offset 1,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 21 ' and value ' Message #21 '
+{:topic-name "test3",
+ :partition 0,
+ :timestamp 1606555678979,
+ :offset 1,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 22 ' and value ' Message #22 '
+{:topic-name "test3",
+ :partition 7,
+ :timestamp 1606555678983,
+ :offset 2,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 23 ' and value ' Message #23 '
+{:topic-name "test3",
+ :partition 2,
+ :timestamp 1606555678987,
+ :offset 2,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 24 ' and value ' Message #24 '
+{:topic-name "test3",
+ :partition 5,
+ :timestamp 1606555678991,
+ :offset 2,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 25 ' and value ' Message #25 '
+{:topic-name "test3",
+ :partition 7,
+ :timestamp 1606555678994,
+ :offset 3,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 26 ' and value ' Message #26 '
+{:topic-name "test3",
+ :partition 2,
+ :timestamp 1606555678998,
+ :offset 3,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 27 ' and value ' Message #27 '
+{:topic-name "test3",
+ :partition 4,
+ :timestamp 1606555679002,
+ :offset 0,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 28 ' and value ' Message #28 '
+{:topic-name "test3",
+ :partition 5,
+ :timestamp 1606555679006,
+ :offset 3,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 29 ' and value ' Message #29 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555679009,
+ :offset 3,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 30 ' and value ' Message #30 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555679013,
+ :offset 4,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 31 ' and value ' Message #31 '
+{:topic-name "test3",
+ :partition 5,
+ :timestamp 1606555679016,
+ :offset 4,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 32 ' and value ' Message #32 '
+{:topic-name "test3",
+ :partition 7,
+ :timestamp 1606555679020,
+ :offset 4,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 33 ' and value ' Message #33 '
+{:topic-name "test3",
+ :partition 7,
+ :timestamp 1606555679023,
+ :offset 5,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 34 ' and value ' Message #34 '
+{:topic-name "test3",
+ :partition 0,
+ :timestamp 1606555679027,
+ :offset 2,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 35 ' and value ' Message #35 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555679030,
+ :offset 5,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 36 ' and value ' Message #36 '
+{:topic-name "test3",
+ :partition 0,
+ :timestamp 1606555679034,
+ :offset 3,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 37 ' and value ' Message #37 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555679037,
+ :offset 6,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 38 ' and value ' Message #38 '
+{:topic-name "test3",
+ :partition 6,
+ :timestamp 1606555679041,
+ :offset 1,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 39 ' and value ' Message #39 '
+{:topic-name "test3",
+ :partition 7,
+ :timestamp 1606555679044,
+ :offset 6,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 40 ' and value ' Message #40 '
+{:topic-name "test3",
+ :partition 9,
+ :timestamp 1606555679048,
+ :offset 2,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 41 ' and value ' Message #41 '
+{:topic-name "test3",
+ :partition 4,
+ :timestamp 1606555679051,
+ :offset 1,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 42 ' and value ' Message #42 '
+{:topic-name "test3",
+ :partition 2,
+ :timestamp 1606555679054,
+ :offset 4,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 43 ' and value ' Message #43 '
+{:topic-name "test3",
+ :partition 4,
+ :timestamp 1606555679058,
+ :offset 2,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 44 ' and value ' Message #44 '
+{:topic-name "test3",
+ :partition 9,
+ :timestamp 1606555679061,
+ :offset 3,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 45 ' and value ' Message #45 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555679065,
+ :offset 7,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 46 ' and value ' Message #46 '
+{:topic-name "test3",
+ :partition 1,
+ :timestamp 1606555679068,
+ :offset 1,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 47 ' and value ' Message #47 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555679072,
+ :offset 8,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 48 ' and value ' Message #48 '
+{:topic-name "test3",
+ :partition 0,
+ :timestamp 1606555679075,
+ :offset 4,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 49 ' and value ' Message #49 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555679078,
+ :offset 9,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 50 ' and value ' Message #50 '
+{:topic-name "test3",
+ :partition 1,
+ :timestamp 1606555679082,
+ :offset 2,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 51 ' and value ' Message #51 '
+{:topic-name "test3",
+ :partition 6,
+ :timestamp 1606555679085,
+ :offset 2,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 52 ' and value ' Message #52 '
+{:topic-name "test3",
+ :partition 2,
+ :timestamp 1606555679089,
+ :offset 5,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 53 ' and value ' Message #53 '
+{:topic-name "test3",
+ :partition 1,
+ :timestamp 1606555679092,
+ :offset 3,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 54 ' and value ' Message #54 '
+{:topic-name "test3",
+ :partition 4,
+ :timestamp 1606555679096,
+ :offset 3,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 55 ' and value ' Message #55 '
+{:topic-name "test3",
+ :partition 0,
+ :timestamp 1606555679099,
+ :offset 5,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 56 ' and value ' Message #56 '
+{:topic-name "test3",
+ :partition 4,
+ :timestamp 1606555679104,
+ :offset 4,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 57 ' and value ' Message #57 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555679108,
+ :offset 6,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 58 ' and value ' Message #58 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555679111,
+ :offset 10,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 59 ' and value ' Message #59 '
+{:topic-name "test3",
+ :partition 7,
+ :timestamp 1606555679114,
+ :offset 7,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 60 ' and value ' Message #60 '
+{:topic-name "test3",
+ :partition 5,
+ :timestamp 1606555679118,
+ :offset 5,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 61 ' and value ' Message #61 '
+{:topic-name "test3",
+ :partition 0,
+ :timestamp 1606555679121,
+ :offset 6,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 62 ' and value ' Message #62 '
+{:topic-name "test3",
+ :partition 0,
+ :timestamp 1606555679125,
+ :offset 7,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 63 ' and value ' Message #63 '
+{:topic-name "test3",
+ :partition 6,
+ :timestamp 1606555679128,
+ :offset 3,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 64 ' and value ' Message #64 '
+{:topic-name "test3",
+ :partition 9,
+ :timestamp 1606555679131,
+ :offset 4,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 65 ' and value ' Message #65 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555679135,
+ :offset 7,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 66 ' and value ' Message #66 '
+{:topic-name "test3",
+ :partition 7,
+ :timestamp 1606555679139,
+ :offset 8,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 67 ' and value ' Message #67 '
+{:topic-name "test3",
+ :partition 2,
+ :timestamp 1606555679142,
+ :offset 6,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 68 ' and value ' Message #68 '
+{:topic-name "test3",
+ :partition 4,
+ :timestamp 1606555679146,
+ :offset 5,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 69 ' and value ' Message #69 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555679149,
+ :offset 11,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 70 ' and value ' Message #70 '
+{:topic-name "test3",
+ :partition 7,
+ :timestamp 1606555679152,
+ :offset 9,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 71 ' and value ' Message #71 '
+{:topic-name "test3",
+ :partition 9,
+ :timestamp 1606555679155,
+ :offset 5,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 72 ' and value ' Message #72 '
+{:topic-name "test3",
+ :partition 0,
+ :timestamp 1606555679158,
+ :offset 8,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 73 ' and value ' Message #73 '
+{:topic-name "test3",
+ :partition 7,
+ :timestamp 1606555679161,
+ :offset 10,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 74 ' and value ' Message #74 '
+{:topic-name "test3",
+ :partition 6,
+ :timestamp 1606555679165,
+ :offset 4,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 75 ' and value ' Message #75 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555679169,
+ :offset 8,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 76 ' and value ' Message #76 '
+{:topic-name "test3",
+ :partition 4,
+ :timestamp 1606555679174,
+ :offset 6,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 77 ' and value ' Message #77 '
+{:topic-name "test3",
+ :partition 5,
+ :timestamp 1606555679179,
+ :offset 6,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 78 ' and value ' Message #78 '
+{:topic-name "test3",
+ :partition 1,
+ :timestamp 1606555679183,
+ :offset 4,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 79 ' and value ' Message #79 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555679187,
+ :offset 9,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 80 ' and value ' Message #80 '
+{:topic-name "test3",
+ :partition 1,
+ :timestamp 1606555679190,
+ :offset 5,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 81 ' and value ' Message #81 '
+{:topic-name "test3",
+ :partition 7,
+ :timestamp 1606555679194,
+ :offset 11,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 82 ' and value ' Message #82 '
+{:topic-name "test3",
+ :partition 6,
+ :timestamp 1606555679197,
+ :offset 5,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 83 ' and value ' Message #83 '
+{:topic-name "test3",
+ :partition 6,
+ :timestamp 1606555679201,
+ :offset 6,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 84 ' and value ' Message #84 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555679205,
+ :offset 10,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 85 ' and value ' Message #85 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555679209,
+ :offset 12,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 86 ' and value ' Message #86 '
+{:topic-name "test3",
+ :partition 6,
+ :timestamp 1606555679212,
+ :offset 7,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 87 ' and value ' Message #87 '
+{:topic-name "test3",
+ :partition 1,
+ :timestamp 1606555679215,
+ :offset 6,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 88 ' and value ' Message #88 '
+{:topic-name "test3",
+ :partition 5,
+ :timestamp 1606555679218,
+ :offset 7,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 89 ' and value ' Message #89 '
+{:topic-name "test3",
+ :partition 6,
+ :timestamp 1606555679221,
+ :offset 8,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 90 ' and value ' Message #90 '
+{:topic-name "test3",
+ :partition 5,
+ :timestamp 1606555679224,
+ :offset 8,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 91 ' and value ' Message #91 '
+{:topic-name "test3",
+ :partition 4,
+ :timestamp 1606555679228,
+ :offset 7,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 92 ' and value ' Message #92 '
+{:topic-name "test3",
+ :partition 9,
+ :timestamp 1606555679230,
+ :offset 6,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 93 ' and value ' Message #93 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555679234,
+ :offset 13,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 94 ' and value ' Message #94 '
+{:topic-name "test3",
+ :partition 6,
+ :timestamp 1606555679237,
+ :offset 9,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 95 ' and value ' Message #95 '
+{:topic-name "test3",
+ :partition 6,
+ :timestamp 1606555679240,
+ :offset 10,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 96 ' and value ' Message #96 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555679244,
+ :offset 11,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 97 ' and value ' Message #97 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555679247,
+ :offset 12,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 98 ' and value ' Message #98 '
+{:topic-name "test3",
+ :partition 8,
+ :timestamp 1606555679250,
+ :offset 14,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 99 ' and value ' Message #99 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555679253,
+ :offset 13,
+ :serialized-key-size 2,
+ :serialized-value-size 11}
+Publishing message with key ' 100 ' and value ' Message #100 '
+{:topic-name "test3",
+ :partition 3,
+ :timestamp 1606555679256,
+ :offset 14,
+ :serialized-key-size 3,
+ :serialized-value-size 12}
---
+++ kafka/clojure/produce-messages-3/project.clj
@@ -0,0 +1,11 @@
+(defproject produce-messages-1 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [fundingcircle/jackdaw "0.7.6"]]
+  :main ^:skip-aot produce-messages-3.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
