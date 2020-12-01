ab4996bf478d8fce2bace713f7bc3e67a07c20f8
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-01 08:01:16 +0100

    Print description of selected topics

---
+++ kafka/clojure/describe-topics/test/describe_topics/core_test.clj
@@ -0,0 +1,7 @@
+(ns describe-topics.core-test
+  (:require [clojure.test :refer :all]
+            [describe-topics.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
