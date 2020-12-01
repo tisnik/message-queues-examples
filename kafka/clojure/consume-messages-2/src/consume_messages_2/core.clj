6d6308e388c5a9ef28c04fc09050d19a4faccc2c
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-01 08:01:16 +0100

    Simplest message consumer, non-default offset policy

---
+++ kafka/clojure/consume-messages-2/src/consume_messages_2/core.clj
@@ -0,0 +1,23 @@
+(ns consume-messages-2.core
+  (:require [jackdaw.client :as jc]
+            [jackdaw.client.log :as jl]
+            [clojure.pprint :as pp]))
+
+(def consumer-config
+  {"bootstrap.servers" "localhost:9092"
+   "key.deserializer" "org.apache.kafka.common.serialization.StringDeserializer"
+   "value.deserializer" "org.apache.kafka.common.serialization.StringDeserializer"
+   "auto.offset.reset" "earliest"
+   "group.id"  "group-A"})
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (with-open [consumer (-> (jc/consumer consumer-config)
+                           (jc/subscribe [{:topic-name "test1"}]))]
+    (doseq [{:keys [key value partition timestamp offset]} (jl/log consumer 10)]
+      (println "key: " key)
+      (println "value: " value)
+      (println "partition: " partition)
+      (println "timestamp: " timestamp)
+      (println "offset: " offset))))
---
+++ kafka/clojure/consume-messages-2/test/consume_messages_2/core_test.clj
@@ -0,0 +1,7 @@
+(ns consume-messages-2.core-test
+  (:require [clojure.test :refer :all]
+            [consume-messages-2.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
