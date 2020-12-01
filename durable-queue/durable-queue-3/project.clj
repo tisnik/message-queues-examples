d8983bd75f7276ce8b0b5f383f6173a24516fd50
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-01 12:10:57 +0100

    Bump up Clojure version to 1.10.1

--- durable-queue/durable-queue-3/project.clj
+++ durable-queue/durable-queue-3/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [factual/durable-queue "0.1.5"]]
   :main ^:skip-aot durable-queue-3.core
   :target-path "target/%s"
