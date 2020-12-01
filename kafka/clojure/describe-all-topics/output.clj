37d1fae10d454647e75ef2fb9830e2fe26c27026
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-01 08:01:16 +0100

    Print description of all topics

---
+++ kafka/clojure/describe-all-topics/output.clj
@@ -0,0 +1,57 @@
+{"test1"
+ {:is-internal? false,
+  :partition-info
+  ({:isr [{:host "localhost", :port 9092, :id 0, :rack nil}],
+    :leader {:host "localhost", :port 9092, :id 0, :rack nil},
+    :partition 0,
+    :replicas [{:host "localhost", :port 9092, :id 0, :rack nil}]})},
+ "test2"
+ {:is-internal? false,
+  :partition-info
+  ({:isr [{:host "localhost", :port 9092, :id 0, :rack nil}],
+    :leader {:host "localhost", :port 9092, :id 0, :rack nil},
+    :partition 0,
+    :replicas [{:host "localhost", :port 9092, :id 0, :rack nil}]})},
+ "test3"
+ {:is-internal? false,
+  :partition-info
+  ({:isr [{:host "localhost", :port 9092, :id 0, :rack nil}],
+    :leader {:host "localhost", :port 9092, :id 0, :rack nil},
+    :partition 0,
+    :replicas [{:host "localhost", :port 9092, :id 0, :rack nil}]}
+   {:isr [{:host "localhost", :port 9092, :id 0, :rack nil}],
+    :leader {:host "localhost", :port 9092, :id 0, :rack nil},
+    :partition 1,
+    :replicas [{:host "localhost", :port 9092, :id 0, :rack nil}]}
+   {:isr [{:host "localhost", :port 9092, :id 0, :rack nil}],
+    :leader {:host "localhost", :port 9092, :id 0, :rack nil},
+    :partition 2,
+    :replicas [{:host "localhost", :port 9092, :id 0, :rack nil}]}
+   {:isr [{:host "localhost", :port 9092, :id 0, :rack nil}],
+    :leader {:host "localhost", :port 9092, :id 0, :rack nil},
+    :partition 3,
+    :replicas [{:host "localhost", :port 9092, :id 0, :rack nil}]}
+   {:isr [{:host "localhost", :port 9092, :id 0, :rack nil}],
+    :leader {:host "localhost", :port 9092, :id 0, :rack nil},
+    :partition 4,
+    :replicas [{:host "localhost", :port 9092, :id 0, :rack nil}]}
+   {:isr [{:host "localhost", :port 9092, :id 0, :rack nil}],
+    :leader {:host "localhost", :port 9092, :id 0, :rack nil},
+    :partition 5,
+    :replicas [{:host "localhost", :port 9092, :id 0, :rack nil}]}
+   {:isr [{:host "localhost", :port 9092, :id 0, :rack nil}],
+    :leader {:host "localhost", :port 9092, :id 0, :rack nil},
+    :partition 6,
+    :replicas [{:host "localhost", :port 9092, :id 0, :rack nil}]}
+   {:isr [{:host "localhost", :port 9092, :id 0, :rack nil}],
+    :leader {:host "localhost", :port 9092, :id 0, :rack nil},
+    :partition 7,
+    :replicas [{:host "localhost", :port 9092, :id 0, :rack nil}]}
+   {:isr [{:host "localhost", :port 9092, :id 0, :rack nil}],
+    :leader {:host "localhost", :port 9092, :id 0, :rack nil},
+    :partition 8,
+    :replicas [{:host "localhost", :port 9092, :id 0, :rack nil}]}
+   {:isr [{:host "localhost", :port 9092, :id 0, :rack nil}],
+    :leader {:host "localhost", :port 9092, :id 0, :rack nil},
+    :partition 9,
+    :replicas [{:host "localhost", :port 9092, :id 0, :rack nil}]})}}
---
+++ kafka/clojure/describe-all-topics/src/describe_topics/core.clj
@@ -0,0 +1,8 @@
+(ns describe-topics.core
+  (:require [jackdaw.admin :as ja]
+            [clojure.pprint :as pp]))
+
+(defn -main
+  [& args]
+  (let [client (ja/->AdminClient {"bootstrap.servers" "localhost:9092"})]
+    (pp/pprint (ja/describe-topics client))))
