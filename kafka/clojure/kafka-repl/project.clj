52a968adceb22263a77600a29604fd1bafefd43a
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-07 08:27:49 +0100

    New dev dependencies: kibit + marginalia

--- kafka/clojure/kafka-repl/project.clj
+++ kafka/clojure/kafka-repl/project.clj
@@ -5,6 +5,8 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [fundingcircle/jackdaw "0.7.6"]]
+  :plugins [[lein-kibit "0.1.8"]
+            [lein-marginalia "0.9.1"]]
   :main ^:skip-aot kafka-repl.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
