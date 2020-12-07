1552e1afee161cee16fccbad358057524809714b
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-07 08:28:46 +0100

    Producer with JSON serdes

---
+++ kafka/clojure/json-serializer/test/json_serializer/core_test.clj
@@ -0,0 +1,7 @@
+(ns json-serializer.core-test
+  (:require [clojure.test :refer :all]
+            [json-serializer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
