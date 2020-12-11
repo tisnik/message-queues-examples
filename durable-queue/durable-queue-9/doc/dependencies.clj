616759baf0758b33b5508ec7cd1e0c3d99ed56fe
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-11 09:12:49 +0100

    Added dependency tree into documentation

---
+++ durable-queue/durable-queue-9/doc/dependencies.clj
@@ -0,0 +1,32 @@
+{[clojure-complete "0.2.5" :exclusions [[org.clojure/clojure]]] nil,
+ [factual/durable-queue "0.1.5"]
+ {[byte-streams "0.2.0"]
+  {[clj-tuple "0.2.1"] nil,
+   [manifold "0.1.0"]
+   {[org.clojure/tools.logging "0.3.1"] nil, [riddley "0.1.9"] nil}},
+  [com.taoensso/nippy "2.8.0"]
+  {[com.taoensso/encore "1.21.0"] nil,
+   [net.jpountz.lz4/lz4 "1.3"] nil,
+   [org.clojure/tools.reader "0.8.13"] nil,
+   [org.iq80.snappy/snappy "0.3"] nil,
+   [org.tukaani/xz "1.5"] nil},
+  [primitive-math "0.1.4"] nil},
+ [nrepl "0.7.0" :exclusions [[org.clojure/clojure]]] nil,
+ [org.clojure/clojure "1.10.1"]
+ {[org.clojure/core.specs.alpha "0.2.44"] nil,
+  [org.clojure/spec.alpha "0.2.176"] nil},
+ [venantius/ultra "0.6.0"]
+ {[grimradical/clj-semver "0.3.0" :exclusions [[org.clojure/clojure]]]
+  nil,
+  [io.aviso/pretty "0.1.35"] nil,
+  [mvxcvi/puget "1.1.0"]
+  {[fipp "0.6.14"] {[org.clojure/core.rrb-vector "0.0.13"] nil},
+   [mvxcvi/arrangement "1.1.1"] nil},
+  [mvxcvi/whidbey "2.1.0"] {[org.clojure/data.codec "0.1.1"] nil},
+  [org.clojars.brenton/google-diff-match-patch "0.1"] nil,
+  [robert/hooke "1.3.0"] nil,
+  [venantius/glow "0.1.5" :exclusions [[hiccup] [garden]]]
+  {[clj-antlr "0.2.3"]
+   {[org.antlr/antlr4-runtime "4.5.3"] nil,
+    [org.antlr/antlr4 "4.5.3"] nil},
+   [instaparse "1.4.1"] nil}}}
