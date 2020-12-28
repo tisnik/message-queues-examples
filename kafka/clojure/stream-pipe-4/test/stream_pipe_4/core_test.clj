891cfa1f3c8e90b96910265e1d394aebf9d52018
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-28 10:23:51 +0100

    Stream pipe #4

---
+++ kafka/clojure/stream-pipe-4/test/stream_pipe_4/core_test.clj
@@ -0,0 +1,7 @@
+(ns stream-pipe-4.core-test
+  (:require [clojure.test :refer :all]
+            [stream-pipe-4.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
