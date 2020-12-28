fac9dfbf66dfca82606cb9369268b0e845b77bea
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-28 10:23:44 +0100

    Stream pipe #2

---
+++ kafka/clojure/stream-pipe-2/project.clj
@@ -0,0 +1,18 @@
+(defproject stream-pipe-2 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [fundingcircle/jackdaw "0.7.6"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
+                                                    javax.jms/jms
+                                                    com.sun.jmdk/jmxtools
+                                                    com.sun.jmx/jmxri]]
+                 [org.slf4j/slf4j-log4j12 "1.6.6"]]
+  :main ^:skip-aot stream-pipe-2.core
+  :target-path "target/%s"
+  :resource-paths ["resources/"]
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
---
+++ kafka/clojure/stream-pipe-2/test/stream_pipe_2/core_test.clj
@@ -0,0 +1,7 @@
+(ns stream-pipe-2.core-test
+  (:require [clojure.test :refer :all]
+            [stream-pipe-1.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
