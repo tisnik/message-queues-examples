ee1358fee26eb0025be685051c13a55e933e1f88
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-12-28 10:24:20 +0100

    Stream pipe #1

--- kafka/clojure/stream-pipe-1/src/stream_pipe_1/core.clj
+++ kafka/clojure/stream-pipe-1/src/stream_pipe_1/core.clj
@@ -4,11 +4,12 @@
             [jackdaw.client.log :as jl]
             [jackdaw.serdes.json]
             [jackdaw.streams :as j]
-            [clojure.pprint :as pp]))
+            [clojure.pprint :as pp]
+            [clojure.tools.logging :as log]))
 
 
-;; konfigurace temat
 (def topic-config
+  "Konfigurace témat - vstupního i výstupního."
   {:input
    {:topic-name "input"
     :partition-count 1
@@ -23,81 +24,85 @@
     :value-serde (jackdaw.serdes.json/serde)}})
 
 
-;; konfigurace aplikace
 (def app-config
+  "Konfigurace aplikace (ve smyslu knihovny Jackdaw)."
   {"application.id" "pipe"
    "bootstrap.servers" "localhost:9092"
    "cache.max.bytes.buffering" "0"})
 
 
 (defn delete-topic
+  "Pomocná funkce pro smazání vybraného tématu."
   [broker-config topic]
   (try
+    (log/warn "Deleting topic" (:topic-name topic))
     (let [client (ja/->AdminClient broker-config)]
       (ja/delete-topics! client [topic]))
     (catch Exception e (str "caught exception: " (.getMessage e)))))
 
 
 (defn new-topic
+  "Pomocná funkce pro vytvoření nového tématu."
   [broker-config topic]
   (try
+    (log/warn "Creating topic" (:topic-name topic))
     (let [client (ja/->AdminClient broker-config)]
       (ja/create-topics! client [topic]))
       (catch Exception e (str "caught exception: " (.getMessage e)))))
 
 
-;; transformacni funkce
 (defn etl
+  "Transformační funkce."
   [[k v]]
   [k {:result (+ (:x v) (:y v))}])
 
 
-;; cela pipeline (kolona)
 (defn build-topology
+  "Definice celé pipeliny (kolony) - základ aplikace."
   [builder topic-config]
   (-> (j/kstream builder (:input topic-config))
       (j/peek (fn [[k v]]
-                (println "Received message with key:   " k " and value:" v)))
+                (log/warn "Received message with key:   " k " and value:" v)))
       (j/map etl)
       (j/peek (fn [[k v]]
-                (println "Transformed message with key:" k " and value:" v)))
+                (log/warn "Transformed message with key:" k " and value:" v)))
       (j/to (:output topic-config)))
   builder)
 
 
-;; spusteni aplikace
 (defn start-app
-  "Starts the stream processing application."
+  "Spuštění aplikace."
   [app-config topic-config]
   (let [builder (j/streams-builder)
         topology (build-topology builder topic-config)
         app (j/kafka-streams topology app-config)]
+    (log/warn "Starting pipe")
     (j/start app)
-    (println "pipe is up")
+    (log/warn "Pipe is up")
     app))
 
 
-;; zastaveni aplikace
 (defn stop-app
-  "Stops the stream processing application."
+  "Zastavení aplikace."
   [app]
+  (log/warn "Stopping pipe")
   (j/close app)
-  (println "pipe is down"))
+  (log/warn "Pipe is down"))
 
 
 (defn -main
   [& args]
   (let [broker-config {"bootstrap.servers" "localhost:9092"}]
 
-    ;; na zacatku pro jistotu vymazeme temata pouzivane pipou
+    ;; na začátku pro jistotu vymažeme témata používaná pipou
     (delete-topic broker-config (:input topic-config))
     (delete-topic broker-config (:output topic-config))
 
-    ;; vytvoreni novych tamat akceptujiciho zpravy ve formatu JSON
+    ;; vytvoření nových témat akceptujících zprávy ve formátu JSON
     (new-topic broker-config (:input topic-config))
     (new-topic broker-config (:output topic-config))
 
-    ;; spusteni kolony
-    (println "Starting pipe")
+    ;; spuštění kolony
+    (log/warn "Starting application")
     (let [app (start-app app-config topic-config)]
-      (println "App:" app))))
+      (log/warn "App created:" app))))
