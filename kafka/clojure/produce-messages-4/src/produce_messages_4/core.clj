dc0292aae3db3af32531eb32de55a004ab609fa0
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-23 11:42:15 +0200

    Message producer #4

---
+++ kafka/clojure/produce-messages-4/src/produce_messages_4/core.clj
@@ -0,0 +1,22 @@
+(ns produce-messages-4.core
+  (:require [jackdaw.client :as jc]
+            [clojure.pprint :as pp]))
+
+(def producer-config
+  {"bootstrap.servers" "localhost:9092"
+   "key.serializer" "org.apache.kafka.common.serialization.StringSerializer"
+   "value.serializer" "org.apache.kafka.common.serialization.StringSerializer"
+   "acks" "all"
+   "client.id" "foo"})
+
+(defn -main
+  [& args]
+  (with-open [producer (jc/producer producer-config)]
+    (doseq [i (range 1 1000)]
+      (Thread/sleep 1000)
+      (let [key (str i)
+            value (str "Message #" i)]
+        (println "Publishing message with key '" key "' and value '" value "'")
+        (let [record-metadata (jc/produce! producer {:topic-name "test3"} key value)]
+          (pp/pprint @record-metadata)))
+      )))
---
+++ kafka/clojure/produce-messages-4/test/produce_messages_4/core_test.clj
@@ -0,0 +1,7 @@
+(ns produce-messages-4.core-test
+  (:require [clojure.test :refer :all]
+            [produce-messages-3.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
