77a9cbd483b2a2cb47e3ccacbb88c266237e4737
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-06-23 11:42:06 +0200

    Message consumer #3

---
+++ kafka/clojure/consume-messages-3/src/consume_messages_3/core.clj
@@ -0,0 +1,22 @@
+(ns consume-messages-3.core
+  (:require [jackdaw.client :as jc]
+            [jackdaw.client.log :as jl]
+            [clojure.pprint :as pp]))
+
+(def consumer-config
+  {"bootstrap.servers" "localhost:9092"
+   "key.deserializer" "org.apache.kafka.common.serialization.StringDeserializer"
+   "value.deserializer" "org.apache.kafka.common.serialization.StringDeserializer"
+   "auto.offset.reset" "earliest"
+   "group.id"  "group-A"})
+
+(defn -main
+  [& args]
+  (with-open [consumer (-> (jc/consumer consumer-config)
+                           (jc/subscribe [{:topic-name "test3"}]))]
+    (doseq [{:keys [key value partition timestamp offset]} (jl/log consumer 10)]
+      (println "key: " key)
+      (println "value: " value)
+      (println "partition: " partition)
+      (println "timestamp: " timestamp)
+      (println "offset: " offset))))
