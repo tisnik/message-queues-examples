6bd6b00b6fceadb4c4ba5082b52228eb466c7bdf
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-07-05 09:08:16 +0200

    Added license header into edn-2-serializer/test/edn_2_serializer/core_test.clj

--- kafka/clojure/edn-2-serializer/test/edn_2_serializer/core_test.clj
+++ kafka/clojure/edn-2-serializer/test/edn_2_serializer/core_test.clj
@@ -1,3 +1,15 @@
+;
+;  (C) Copyright 2021  Pavel Tisnovsky
+;
+;  All rights reserved. This program and the accompanying materials
+;  are made available under the terms of the Eclipse Public License v1.0
+;  which accompanies this distribution, and is available at
+;  http://www.eclipse.org/legal/epl-v10.html
+;
+;  Contributors:
+;      Pavel Tisnovsky
+;
+
 (ns edn-2-serializer.core-test
   (:require [clojure.test :refer :all]
             [edn-2-serializer.core :refer :all]))
