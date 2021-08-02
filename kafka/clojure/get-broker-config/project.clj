ce396c0515bedcbb73e581163c83a4140a2372fc
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-02 08:38:10 +0200

    New plugin test2junit added into get-broker-config/project.clj

--- kafka/clojure/get-broker-config/project.clj
+++ kafka/clojure/get-broker-config/project.clj
@@ -18,6 +18,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [fundingcircle/jackdaw "0.7.6"]]
   :plugins [[lein-codox "0.10.7"]
+            [test2junit "1.1.0"]
             ]
   :main ^:skip-aot get-broker-config.core
   :target-path "target/%s"
