232c26da3fc07b75127322b9eb29261936588ff8
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-03 07:53:43 +0200

    New plugin lein-cloverage added into topic-destructor/project.clj

--- kafka/clojure/topic-destructor/project.clj
+++ kafka/clojure/topic-destructor/project.clj
@@ -18,6 +18,8 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [fundingcircle/jackdaw "0.7.6"]]
   :plugins [[lein-codox "0.10.7"]
+            [test2junit "1.1.0"]
+            [lein-cloverage "1.0.7-SNAPSHOT"]
             ]
   :main ^:skip-aot topic-destructor.core
   :target-path "target/%s"
