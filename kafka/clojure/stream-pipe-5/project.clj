034674b757c63cc24955318f6380c70ea7130404
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-04 08:33:29 +0200

    New plugin lein-kibit added into stream-pipe-5/project.clj

--- kafka/clojure/stream-pipe-5/project.clj
+++ kafka/clojure/stream-pipe-5/project.clj
@@ -26,6 +26,7 @@
   :plugins [[lein-codox "0.10.7"]
             [test2junit "1.1.0"]
             [lein-cloverage "1.0.7-SNAPSHOT"]
+            [lein-kibit "0.1.8"]
             ]
   :main ^:skip-aot stream-pipe-5.core
   :target-path "target/%s"
