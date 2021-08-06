5c85446d4e0383baf8e5ae529bc0814e9c7e1b01
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-06 07:46:20 +0200

    New plugin lein-project-edn added into json-serializer/project.clj

--- kafka/clojure/json-serializer/project.clj
+++ kafka/clojure/json-serializer/project.clj
@@ -22,6 +22,7 @@
             [lein-cloverage "1.0.7-SNAPSHOT"]
             [lein-kibit "0.1.8"]
             [lein-clean-m2 "0.1.2"]
+            [lein-project-edn "0.3.0"]
             ]
   :main ^:skip-aot json-serializer.core
   :target-path "target/%s"
