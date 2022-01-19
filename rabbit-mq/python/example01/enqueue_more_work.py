44939c629866d1dfce4056389001ff4930c01443
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-19 11:21:42 +0100

    Reformatted

--- rabbit-mq/python/example01/enqueue_more_work.py
+++ rabbit-mq/python/example01/enqueue_more_work.py
@@ -3,15 +3,15 @@
 
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 channel = connection.channel()
 
-channel.queue_declare(queue='test')
+channel.queue_declare(queue="test")
 
 for i in range(1, 11):
-    channel.basic_publish(exchange='',
-                          routing_key='test',
-                          body='Hello World! #{i}'.format(i=i))
+    channel.basic_publish(
+        exchange="", routing_key="test", body="Hello World! #{i}".format(i=i)
+    )
 
 print("Sent 'Hello World!' ten times")
 connection.close()
