587a46cb100ad40e588621e48229264c693dd295
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-19 11:20:09 +0100

    Reformatted

--- kafka/producer1.py
+++ kafka/producer1.py
@@ -4,15 +4,16 @@ from kafka import KafkaProducer
 from time import sleep
 from json import dumps
 
-server = 'localhost:9092'
-topic = 'upload'
+server = "localhost:9092"
+topic = "upload"
 
-print('Connecting to Kafka')
-producer = KafkaProducer(bootstrap_servers=[server],
-                         value_serializer=lambda x: dumps(x).encode('utf-8'))
-print('Connected to Kafka')
+print("Connecting to Kafka")
+producer = KafkaProducer(
+    bootstrap_servers=[server], value_serializer=lambda x: dumps(x).encode("utf-8")
+)
+print("Connected to Kafka")
 
 for i in range(1000):
-    data = {'counter': i}
+    data = {"counter": i}
     producer.send(topic, value=data)
     sleep(5)
