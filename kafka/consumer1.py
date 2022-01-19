c1f1a75b553093c35668196ecc846d4f39f2963e
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-19 11:19:57 +0100

    Reformatted

--- kafka/consumer1.py
+++ kafka/consumer1.py
@@ -3,20 +3,27 @@
 import sys
 from kafka import KafkaConsumer
 
-server = 'localhost:9092'
-topic = 'upload'
-group_id = 'group1'
+server = "localhost:9092"
+topic = "upload"
+group_id = "group1"
 
-print('Connecting to Kafka')
-consumer = KafkaConsumer(topic,
-                         group_id=group_id,
-                         bootstrap_servers=[server],
-                         auto_offset_reset='earliest')
-print('Connected to Kafka')
+print("Connecting to Kafka")
+consumer = KafkaConsumer(
+    topic, group_id=group_id, bootstrap_servers=[server], auto_offset_reset="earliest"
+)
+print("Connected to Kafka")
 
 try:
     for message in consumer:
-        print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                             message.offset, message.key, message.value))
+        print(
+            "%s:%d:%d: key=%s value=%s"
+            % (
+                message.topic,
+                message.partition,
+                message.offset,
+                message.key,
+                message.value,
+            )
+        )
 except KeyboardInterrupt:
     sys.exit()
