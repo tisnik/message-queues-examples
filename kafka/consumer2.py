ca129e6e0ed25a1a3c326eb4a92d9467085ba868
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-01-19 11:20:03 +0100

    Reformatted

--- kafka/consumer2.py
+++ kafka/consumer2.py
@@ -3,14 +3,13 @@
 import sys
 from kafka import KafkaConsumer, TopicPartition
 
-server = 'localhost:9092'
-topic = 'upload'
-group_id = 'group1'
+server = "localhost:9092"
+topic = "upload"
+group_id = "group1"
 
-print('Connecting to Kafka')
-consumer = KafkaConsumer(group_id=group_id,
-                         bootstrap_servers=[server])
-print('Connected to Kafka')
+print("Connecting to Kafka")
+consumer = KafkaConsumer(group_id=group_id, bootstrap_servers=[server])
+print("Connected to Kafka")
 
 tp = TopicPartition(topic=topic, partition=0)
 consumer.assign([tp])
@@ -18,7 +17,15 @@ consumer.seek(tp, 0)
 
 try:
     for message in consumer:
-        print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                             message.offset, message.key, message.value))
+        print(
+            "%s:%d:%d: key=%s value=%s"
+            % (
+                message.topic,
+                message.partition,
+                message.offset,
+                message.key,
+                message.value,
+            )
+        )
 except KeyboardInterrupt:
     sys.exit()
