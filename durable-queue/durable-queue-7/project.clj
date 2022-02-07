9bf4876045cab8138b17bda02e71cf2f91c3672b
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-02-07 10:41:25 +0100

    New plugin dependency test2junit

--- durable-queue/durable-queue-7/project.clj
+++ durable-queue/durable-queue-7/project.clj
@@ -18,6 +18,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [factual/durable-queue "0.1.5"]]
   :plugins [[lein-codox "0.10.7"]
+            [test2junit "1.1.0"]
             ]
   :main ^:skip-aot durable-queue-7.core
   :project-edn {:output-file "doc/details.clj"}
