a82aa241c532aa0b36bfac339db133b3379934d9
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-09-23 07:29:02 +0200

    Reformatted by Black

--- celery/example02/tasks.py
+++ celery/example02/tasks.py
@@ -10,9 +10,9 @@
 
 from celery import Celery
 
-app = Celery('tasks',
-             broker='redis://localhost:6379/0',
-             backend='redis://localhost:6379/0')
+app = Celery(
+    "tasks", broker="redis://localhost:6379/0", backend="redis://localhost:6379/0"
+)
 
 
 @app.task
