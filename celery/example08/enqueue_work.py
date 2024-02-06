c176f655d7dd6f04ca6a9512c0a804d6230c6e78
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-06 08:37:01 +0100

    Fixed order of imports

--- celery/example08/enqueue_work.py
+++ celery/example08/enqueue_work.py
@@ -1,6 +1,6 @@
 # vim: set fileencoding=utf-8
 
-from tasks import red_task, green_task, blue_task
+from tasks import blue_task, green_task, red_task
 
 red_task.apply_async(queue="red_queue")
 green_task.apply_async(queue="green_queue")
