8c0df36548812d4ddb1c53462339e4b9e0c6b1eb
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-14 08:12:13 +0100

    Removed unused imports, fixes exception handlers

--- dramatiq/example07/enqueue_work.py
+++ dramatiq/example07/enqueue_work.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # vim: set fileencoding=utf-8
 
-import time
 
 from test_worker_7 import setup_broker, test_worker_A, test_worker_B, test_worker_C
 
 import dramatiq
-from dramatiq.brokers.redis import RedisBroker
 
 setup_broker()
 
