13345ddb1e2158f0c2dc70fe469849b27c617a7b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-15 08:24:32 +0100

    Better contexts handling

--- 0mq/python/example08_context_managers/client.py
+++ 0mq/python/example08_context_managers/client.py
@@ -7,16 +7,15 @@ PORT = 5556
 def start_client():
     """Spuštění klienta."""
 
-    with zmq.Context() as context:
-        with context.socket(CONNECTION_TYPE) as socket:
-            address = "tcp://localhost:{port}".format(port=PORT)
-            socket.connect(address)
-            print("Connected to {a}".format(a=address))
+    with zmq.Context() as context, context.socket(CONNECTION_TYPE) as socket:
+        address = "tcp://localhost:{port}".format(port=PORT)
+        socket.connect(address)
+        print("Connected to {a}".format(a=address))
 
-            print("Waiting for message...")
-            while True:
-                message = socket.recv_string()
-                print("Received message '{m}'".format(m=message))
+        print("Waiting for message...")
+        while True:
+            message = socket.recv_string()
+            print("Received message '{m}'".format(m=message))
 
 
 start_client()
