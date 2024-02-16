7ea7430e59965b4b1c4d805e850c97ab53ce314a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-16 08:42:03 +0100

    Reformatted by Black

--- dramatiq/example08/enqueue_work.py
+++ dramatiq/example08/enqueue_work.py
@@ -2,7 +2,12 @@
 # vim: set fileencoding=utf-8
 
 
-from test_worker_8 import setup_broker_and_backend, test_worker_A, test_worker_B, test_worker_C
+from test_worker_8 import (
+    setup_broker_and_backend,
+    test_worker_A,
+    test_worker_B,
+    test_worker_C,
+)
 
 import dramatiq
 
@@ -10,9 +15,11 @@ setup_broker_and_backend()
 
 for i in range(1, 6):
     print(i)
-    g = dramatiq.group([
-        test_worker_A.message(str(i)),
-        test_worker_B.message(str(i)),
-        test_worker_C.message(str(i))
-    ]).run()
+    g = dramatiq.group(
+        [
+            test_worker_A.message(str(i)),
+            test_worker_B.message(str(i)),
+            test_worker_C.message(str(i)),
+        ]
+    ).run()
     g.wait(timeout=20000)
