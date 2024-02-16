f6d3086d4ba9b1f155307806b22dabfef8737b82
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-16 08:42:02 +0100

    Reformatted by Black

--- amq/example05_ack/subscriber.py
+++ amq/example05_ack/subscriber.py
@@ -6,15 +6,14 @@ import stomp
 
 
 class SimpleListener:
-
     def __init__(self, conn):
         self.conn = conn
 
     def on_message(self, headers, message):
         print("Received message: {m}".format(m=message))
         print(headers)
-        message_id = headers['message-id']
-        subscription = headers['subscription']
+        message_id = headers["message-id"]
+        subscription = headers["subscription"]
         self.conn.ack(message_id, subscription)
 
     def on_error(self, headers, message):
@@ -24,11 +23,11 @@ class SimpleListener:
 destination = "/queue/test"
 
 conn = stomp.Connection(host_and_ports=[("localhost", 61613)])
-conn.set_listener('', SimpleListener(conn))
+conn.set_listener("", SimpleListener(conn))
 conn.start()
 
 conn.connect(login="admin", passcode="admin")
-conn.subscribe(id='simple_listener', destination=destination, ack='client')
+conn.subscribe(id="simple_listener", destination=destination, ack="client")
 
 print("Waiting for messages...")
 
