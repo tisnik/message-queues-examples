454cd83383a766e2852b58a9638dced77a7485a0
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-16 08:42:03 +0100

    Reformatted by Black

--- dramatiq/example09/enqueue_work.py
+++ dramatiq/example09/enqueue_work.py
@@ -2,16 +2,19 @@
 # vim: set fileencoding=utf-8
 
 
-from test_worker_9 import setup_broker_and_backend, test_worker_A, test_worker_B, test_worker_C
+from test_worker_9 import (
+    setup_broker_and_backend,
+    test_worker_A,
+    test_worker_B,
+    test_worker_C,
+)
 
 import dramatiq
 
 setup_broker_and_backend()
 
-p = dramatiq.pipeline([
-    test_worker_A.message("!"),
-    test_worker_B.message(),
-    test_worker_C.message()
-]).run()
+p = dramatiq.pipeline(
+    [test_worker_A.message("!"), test_worker_B.message(), test_worker_C.message()]
+).run()
 
 print(p.get_result(block=True, timeout=5000))
