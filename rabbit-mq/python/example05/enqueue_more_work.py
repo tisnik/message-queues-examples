24ad01e88a27d90d3be21ca7fa9879b6a7bddb38
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-19 08:13:35 +0100

    Reformatted by Black

--- rabbit-mq/python/example05/enqueue_more_work.py
+++ rabbit-mq/python/example05/enqueue_more_work.py
@@ -4,9 +4,9 @@ from rabbitmq_connect import connect
 connection, channel = connect()
 
 for i in range(1, 11):
-    channel.basic_publish(exchange='',
-                          routing_key='test',
-                          body='Hello World! #{i}'.format(i=i))
+    channel.basic_publish(
+        exchange="", routing_key="test", body="Hello World! #{i}".format(i=i)
+    )
 
 print("Sent 'Hello World!' ten times")
 connection.close()
