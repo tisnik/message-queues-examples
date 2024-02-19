153442e9099c8497d85f31b36d4d5a3f7c704f11
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-19 08:13:35 +0100

    Reformatted by Black

--- rabbit-mq/python/example07/enqueue_more_work.py
+++ rabbit-mq/python/example07/enqueue_more_work.py
@@ -7,12 +7,12 @@ def run_producer(queue_name):
     channel = open_channel(connection)
 
     for i in range(1, 11):
-        channel.basic_publish(exchange='',
-                              routing_key=queue_name,
-                              body='Hello World! #{i}'.format(i=i))
+        channel.basic_publish(
+            exchange="", routing_key=queue_name, body="Hello World! #{i}".format(i=i)
+        )
 
-    print('Sent \'Hello World!\' ten times into the queue "{q}"'.format(q=queue_name))
+    print("Sent 'Hello World!' ten times into the queue \"{q}\"".format(q=queue_name))
     connection.close()
 
 
-run_producer('test')
+run_producer("test")
