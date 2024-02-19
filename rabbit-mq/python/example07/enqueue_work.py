d13d50ea49d280a854c54bb6cc01c1429e973774
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-19 08:13:35 +0100

    Reformatted by Black

--- rabbit-mq/python/example07/enqueue_work.py
+++ rabbit-mq/python/example07/enqueue_work.py
@@ -6,13 +6,11 @@ def run_producer(queue_name):
     connection = connect()
     channel = open_channel(connection, queue_name)
 
-    channel.basic_publish(exchange='',
-                          routing_key=queue_name,
-                          body='Hello World!')
+    channel.basic_publish(exchange="", routing_key=queue_name, body="Hello World!")
 
-    print('Sent \'Hello World!\' message into the queue "{q}"'.format(q=queue_name))
+    print("Sent 'Hello World!' message into the queue \"{q}\"".format(q=queue_name))
 
     connection.close()
 
 
-run_producer('test')
+run_producer("test")
