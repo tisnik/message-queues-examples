087fbbc71283c3887e3916bf3c8d90416d9fa62d
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-19 08:13:35 +0100

    Reformatted by Black

--- rabbit-mq/python/example10/rabbitmq_connect.py
+++ rabbit-mq/python/example10/rabbitmq_connect.py
@@ -3,29 +3,29 @@
 import pika
 
 
-def connect(where='localhost'):
+def connect(where="localhost"):
     return pika.BlockingConnection(pika.ConnectionParameters(where))
 
 
-def open_channel(connection, queue_name='test'):
+def open_channel(connection, queue_name="test"):
     # pokus o nove vytvoreni fronty ve skutecnosti neovlivni jiz existujici frontu
     channel = connection.channel()
     channel.queue_declare(queue=queue_name)
     return channel
 
 
-def use_fanout(channel, exchange_name='fanout_exchange'):
-    channel.exchange_declare(exchange=exchange_name,
-                             exchange_type='fanout')
+def use_fanout(channel, exchange_name="fanout_exchange"):
+    channel.exchange_declare(exchange=exchange_name, exchange_type="fanout")
 
 
-def use_topic_exchange(channel, exchange_name='topic_exchange'):
-    channel.exchange_declare(exchange=exchange_name,
-                             exchange_type='topic')
+def use_topic_exchange(channel, exchange_name="topic_exchange"):
+    channel.exchange_declare(exchange=exchange_name, exchange_type="topic")
 
 
-def bind_queue(channel, queue_name, routing_pattern='', exchange_name='fanout_exchange'):
+def bind_queue(
+    channel, queue_name, routing_pattern="", exchange_name="fanout_exchange"
+):
     channel.queue_declare(queue=queue_name)
-    channel.queue_bind(exchange=exchange_name,
-                       queue=queue_name,
-                       routing_key=routing_pattern)
+    channel.queue_bind(
+        exchange=exchange_name, queue=queue_name, routing_key=routing_pattern
+    )
