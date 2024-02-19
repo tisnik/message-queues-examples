18ef3fed5aaf87360698365fd034acf1b0be11b6
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-19 08:13:35 +0100

    Reformatted by Black

--- rabbit-mq/python/example06/enqueue_work.py
+++ rabbit-mq/python/example06/enqueue_work.py
@@ -4,9 +4,7 @@ from rabbitmq_connect import connect, open_channel
 connection = connect()
 channel = open_channel(connection)
 
-channel.basic_publish(exchange='',
-                      routing_key='test',
-                      body='Hello World!')
+channel.basic_publish(exchange="", routing_key="test", body="Hello World!")
 
 print("Sent 'Hello World!'")
 connection.close()
