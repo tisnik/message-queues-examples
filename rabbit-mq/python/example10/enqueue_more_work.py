f9b5aa89d15b3625bebfb8aa2ac1fdde871d6224
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-19 08:13:35 +0100

    Reformatted by Black

--- rabbit-mq/python/example10/enqueue_more_work.py
+++ rabbit-mq/python/example10/enqueue_more_work.py
@@ -10,23 +10,39 @@ def run_producer():
     channel = open_channel(connection)
 
     use_topic_exchange(channel)
-    bind_queue(channel, 'europe_queue',
-               routing_pattern='europe.*', exchange_name='topic_exchange')
-    bind_queue(channel, 'asia_queue',
-               routing_pattern='asia.*', exchange_name='topic_exchange')
-    bind_queue(channel, 'americas_queue',
-               routing_pattern='americas.*', exchange_name='topic_exchange')
+    bind_queue(
+        channel,
+        "europe_queue",
+        routing_pattern="europe.*",
+        exchange_name="topic_exchange",
+    )
+    bind_queue(
+        channel, "asia_queue", routing_pattern="asia.*", exchange_name="topic_exchange"
+    )
+    bind_queue(
+        channel,
+        "americas_queue",
+        routing_pattern="americas.*",
+        exchange_name="topic_exchange",
+    )
 
-    keys = ("europe.cr", "europe.sr", "europe.pl",
-            "asia.china",
-            "americas.canada", "americas.chile")
+    keys = (
+        "europe.cr",
+        "europe.sr",
+        "europe.pl",
+        "asia.china",
+        "americas.canada",
+        "americas.chile",
+    )
     for key in keys:
         print(key)
-        channel.basic_publish(exchange='topic_exchange',
-                              routing_key=key,
-                              body='Hello World! #{k}'.format(k=key))
+        channel.basic_publish(
+            exchange="topic_exchange",
+            routing_key=key,
+            body="Hello World! #{k}".format(k=key),
+        )
 
-    print('Sent \'Hello World!\' to all selected regions')
+    print("Sent 'Hello World!' to all selected regions")
     connection.close()
 
 
