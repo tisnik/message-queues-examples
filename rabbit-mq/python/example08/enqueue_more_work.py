6d7153d509fb6551dfb6fe715c1e0160cca3d5d2
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-19 08:13:35 +0100

    Reformatted by Black

--- rabbit-mq/python/example08/enqueue_more_work.py
+++ rabbit-mq/python/example08/enqueue_more_work.py
@@ -11,16 +11,16 @@ def run_producer(queue_name):
     channel = open_channel(connection)
 
     for i in range(1, 11):
-        channel.basic_publish(exchange='',
-                              routing_key=queue_name,
-                              body='Hello World! #{i}'.format(i=i))
+        channel.basic_publish(
+            exchange="", routing_key=queue_name, body="Hello World! #{i}".format(i=i)
+        )
 
-    print('Sent \'Hello World!\' ten times into the queue "{q}"'.format(q=queue_name))
+    print("Sent 'Hello World!' ten times into the queue \"{q}\"".format(q=queue_name))
     connection.close()
 
 
 if len(argv) <= 1:
-    print('Please provide queue name on the CLI')
+    print("Please provide queue name on the CLI")
     exit(1)
 
 run_producer(argv[1])
