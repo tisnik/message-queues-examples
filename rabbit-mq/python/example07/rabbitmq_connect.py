b2fbf56b35d207520d978f4fc3593fbaabe98d09
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-19 08:13:35 +0100

    Reformatted by Black

--- rabbit-mq/python/example07/rabbitmq_connect.py
+++ rabbit-mq/python/example07/rabbitmq_connect.py
@@ -1,11 +1,11 @@
 import pika
 
 
-def connect(where='localhost'):
+def connect(where="localhost"):
     return pika.BlockingConnection(pika.ConnectionParameters(where))
 
 
-def open_channel(connection, queue_name='test'):
+def open_channel(connection, queue_name="test"):
     # pokus o nove vytvoreni fronty ve skutecnosti neovlivni jiz existujici frontu
     channel = connection.channel()
     channel.queue_declare(queue=queue_name)
