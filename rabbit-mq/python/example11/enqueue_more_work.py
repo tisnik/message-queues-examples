0b270850eadb4fa8551b3a53eebe7a6d3774632f
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-19 08:13:35 +0100

    Reformatted by Black

--- rabbit-mq/python/example11/enqueue_more_work.py
+++ rabbit-mq/python/example11/enqueue_more_work.py
@@ -8,16 +8,15 @@ from rabbitmq_connect import connect, open_channel
 
 def run_producer():
     connection = connect()
-    channel = open_channel(connection, queue_name='priority_queue_2', max_priority=10)
+    channel = open_channel(connection, queue_name="priority_queue_2", max_priority=10)
 
     for i in range(1, 11):
         priority = 5 * (i % 3)
         prop = BasicProperties(priority=priority)
-        body = 'Hello World! #{i} msg with priority {p}'.format(i=i, p=priority)
-        channel.basic_publish(exchange='',
-                              routing_key='priority_queue_2',
-                              body=body,
-                              properties=prop)
+        body = "Hello World! #{i} msg with priority {p}".format(i=i, p=priority)
+        channel.basic_publish(
+            exchange="", routing_key="priority_queue_2", body=body, properties=prop
+        )
 
     connection.close()
 
