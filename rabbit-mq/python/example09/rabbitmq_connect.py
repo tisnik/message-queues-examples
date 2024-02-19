00fd0b8a639d0737b2e5228413a94a68000dbc57
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-19 08:13:35 +0100

    Reformatted by Black

--- rabbit-mq/python/example09/rabbitmq_connect.py
+++ rabbit-mq/python/example09/rabbitmq_connect.py
@@ -3,23 +3,21 @@
 import pika
 
 
-def connect(where='localhost'):
+def connect(where="localhost"):
     return pika.BlockingConnection(pika.ConnectionParameters(where))
 
 
-def open_channel(connection, queue_name='test'):
+def open_channel(connection, queue_name="test"):
     # pokus o nove vytvoreni fronty ve skutecnosti neovlivni jiz existujici frontu
     channel = connection.channel()
     channel.queue_declare(queue=queue_name)
     return channel
 
 
-def use_fanout(channel, exchange_name='fanout_exchange'):
-    channel.exchange_declare(exchange=exchange_name,
-                             exchange_type='fanout')
+def use_fanout(channel, exchange_name="fanout_exchange"):
+    channel.exchange_declare(exchange=exchange_name, exchange_type="fanout")
 
 
-def bind_queue(channel, queue_name, exchange_name='fanout_exchange'):
+def bind_queue(channel, queue_name, exchange_name="fanout_exchange"):
     channel.queue_declare(queue=queue_name)
-    channel.queue_bind(exchange=exchange_name,
-                       queue=queue_name)
+    channel.queue_bind(exchange=exchange_name, queue=queue_name)
