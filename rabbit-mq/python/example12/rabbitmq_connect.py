6ebf58760520ac3dfe32e4c76159a327b18893cc
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-20 08:17:23 +0100

    Reformatted by Black

--- rabbit-mq/python/example12/rabbitmq_connect.py
+++ rabbit-mq/python/example12/rabbitmq_connect.py
@@ -3,11 +3,11 @@
 import pika
 
 
-def connect(where='localhost'):
+def connect(where="localhost"):
     return pika.BlockingConnection(pika.ConnectionParameters(where))
 
 
-def open_channel(connection, queue_name='test', max_priority=10):
+def open_channel(connection, queue_name="test", max_priority=10):
     # pokus o nove vytvoreni fronty ve skutecnosti neovlivni jiz existujici frontu
     channel = connection.channel()
     channel.queue_declare(queue=queue_name, arguments={"x-max-priority": max_priority})
